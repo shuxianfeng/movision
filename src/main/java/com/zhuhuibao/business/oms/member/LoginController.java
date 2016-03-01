16d54cd19d13c8a527aac12e1056b03f588cfffb
penglong <19630759@qq.com>
2016-03-01 14:09:28 +0800

    数据字典先关类 Signed-off-by: penglong <19630759@qq.com>

---
+++ src/main/java/com/zhuhuibao/business/oms/member/LoginController.java
@@ -0,0 +1,157 @@
+package com.zhuhuibao.business.oms.member;
+
+import java.io.IOException;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.zhuhuibao.common.JsonResult;
+import com.zhuhuibao.mybatis.memberReg.entity.Member;
+import com.zhuhuibao.mybatis.memberReg.service.MemberRegService;
+import com.zhuhuibao.security.EncodeUtil;
+import com.zhuhuibao.utils.JsonUtils;
+import com.zhuhuibao.utils.VerifyCodeUtils;
+
+/**
+ * 运营管理系统登录
+ * @author penglong
+ *
+ */
+public class LoginController {
+	private static final Logger log = LoggerFactory.getLogger(LoginController.class);
+	
+	@Autowired
+	MemberRegService memberService;
+	
+	@RequestMapping(value = "/oms/login", method = RequestMethod.POST)
+    @ResponseBody
+    public void login(HttpServletRequest req,HttpServletResponse response,Member member, Model model) throws JsonGenerationException, JsonMappingException, IOException {
+        log.info("oms login post 登录校验");
+        JsonResult jsonResult = new JsonResult();
+        Subject currentUser = SecurityUtils.getSubject();
+        String username = "";
+        UsernamePasswordToken token = null;
+        try {
+        	String verifyCode = (String) req.getSession().getAttribute("oms");
+			if(verifyCode != null && verifyCode.equalsIgnoreCase(member.getCheckCode()))
+        	{
+				username = member.getAccount();
+		        String pwd = new String(EncodeUtil.decodeBase64(member.getPassword()));
+		        member.setPassword(pwd);
+		        token = new UsernamePasswordToken(username, pwd);
+		        currentUser.login(token);
+		        jsonResult.setData(username);
+        	}
+			else
+			{
+				jsonResult.setCode(400);
+				jsonResult.setMessage("验证码错误");
+			}
+        } catch (UnknownAccountException e) {
+//            e.printStackTrace();
+            jsonResult.setCode(400);
+            jsonResult.setMessage("用户名不存在");
+        }catch (LockedAccountException e){
+            /*e.printStackTrace();
+            model.addAttribute("error", "帐户状态异常");
+            result = "login";*/
+		    jsonResult.setCode(400);
+		    jsonResult.setMessage("帐户状态异常");
+        } catch (AuthenticationException e){
+            /*e.printStackTrace();
+            model.addAttribute("error", "用户名或密码错误");
+            result = "login";*/
+        	jsonResult.setCode(400);
+		    jsonResult.setMessage("用户名或密码错误");
+        }
+
+        if(currentUser.isAuthenticated()){
+            System.out.println("用户[" + username + "]登录认证通过(这里可以进行一些认证通过后的一些系统参数初始化操作)");
+        }else{
+            token.clear();
+        }
+        response.setContentType("application/json;charset=utf-8");
+		response.getWriter().write(JsonUtils.getJsonStringFromObj(jsonResult));
+    }
+    
+    @RequestMapping(value = "/oms/logout", method = RequestMethod.GET)
+    @ResponseBody
+    public void logout(HttpServletResponse response) throws JsonGenerationException, JsonMappingException, IOException
+    {
+        SecurityUtils.getSubject().logout();
+        JsonResult jsonResult = new JsonResult();
+        response.setContentType("application/json;charset=utf-8");
+      	response.getWriter().write(JsonUtils.getJsonStringFromObj(jsonResult));
+    }
+    
+    /**
+	  * 获得图形验证码
+	  * @param req
+	  * @param response
+	  * @param model
+	  */
+	@RequestMapping(value = "/oms/imgCode", method = RequestMethod.GET)
+	public void getCode(HttpServletRequest req, HttpServletResponse response,
+			Model model) {
+		log.debug("获得验证码");
+		getImageVerifyCode(req, response,100,40,4,"oms");
+	}
+    
+    /**
+	 * 生成图片验证码
+	 * @param req
+	 * @param response
+	 * @param imgWidth  图片的宽度
+	 * @param imgheight 图片的高度
+	 * @param verifySize 验证码的长度
+	 * @param key session存储的关键字
+	 */
+	private void getImageVerifyCode(HttpServletRequest req,
+			HttpServletResponse response,int imgWidth,int imgheight,int verifySize,String key) {
+		response.setDateHeader("Expires", 0);
+		response.setHeader("Cache-Control",
+				"no-store, no-cache, must-revalidate");
+		response.addHeader("Cache-Control", "post-check=0, pre-check=0");
+		response.setHeader("Pragma", "no-cache");
+		response.setContentType("image/jpeg");
+		HttpSession sess = req.getSession(true);
+		// 生成随机字串
+		String verifyCode = VerifyCodeUtils.generateVerifyCode(4);
+		log.debug("verifyCode == " + verifyCode);
+		sess.setAttribute(key, verifyCode);
+		ServletOutputStream out = null;
+		try {
+			out = response.getOutputStream();
+			VerifyCodeUtils.outputImage1(imgWidth, imgheight, out, verifyCode);
+			out.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				out.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
