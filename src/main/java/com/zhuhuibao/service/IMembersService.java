14817b29cd4e11f798df1ad8bc8821c70b88e172
caijl <4706540@qq.com>
2016-04-15 19:58:22 +0800

    增加供应商搜索接口

--- src/main/java/com/zhuhuibao/service/IMembersService.java
+++ src/main/java/com/zhuhuibao/service/IMembersService.java
@@ -5,10 +5,13 @@ import java.io.OutputStream;
 import java.util.List;
 import java.util.Map;
 import com.zhuhuibao.pojo.ContractorSearchSpec;
+import com.zhuhuibao.pojo.SupplierSearchSpec;
 import com.zhuhuibao.service.exception.ServiceException;
 
 public interface IMembersService {
 
 	Map<String, Object> searchContractors(ContractorSearchSpec spec) throws ServiceException;
+	
+	Map<String, Object> searchSuppliers(SupplierSearchSpec spec) throws ServiceException;
 
 }
