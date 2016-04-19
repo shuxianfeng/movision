0316349b106582ef523df24dd67cd75791c20a8d
caijl <4706540@qq.com>
2016-04-19 19:03:58 +0800

    获取客户端IP

--- src/main/java/com/zhuhuibao/aop/AspectLog.java
+++ src/main/java/com/zhuhuibao/aop/AspectLog.java
@@ -47,7 +47,7 @@ public class AspectLog {
         	}
         }
         HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest();
-        String clientIP = request.getRemoteAddr();
+        String clientIP = request.getHeader("X-Real-IP").isEmpty()?"127.0.0.1":request.getHeader("X-Real-IP");
         String httpMethod = request.getMethod();
         String requestURL = request.getRequestURL().toString();
         String queryString = request.getQueryString();
