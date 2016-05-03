68426e2b30a3d90ffc2885d0fd10aaaffbfed3cd
jiangliangzhi <jianglz@zhuhui8.com>
2016-05-03 18:07:28 +0800

    3-4

---
+++ src/main/java/com/zhuhuibao/common/util/SwaggerConfig.java
@@ -0,0 +1,55 @@
+package com.zhuhuibao.common.util;
+
+import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
+import com.mangofactory.swagger.models.dto.ApiInfo;
+import com.mangofactory.swagger.plugin.EnableSwagger;
+import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * Swagger配置
+ * @author jianglz
+ * @version 2016-05-03
+ */
+@Configuration
+@EnableSwagger
+@EnableWebMvc
+public class SwaggerConfig {
+    private SpringSwaggerConfig springSwaggerConfig;
+
+    /**
+     * Required to autowire SpringSwaggerConfig
+     */
+    @Autowired
+    public void setSpringSwaggerConfig(SpringSwaggerConfig springSwaggerConfig)
+    {
+        this.springSwaggerConfig = springSwaggerConfig;
+    }
+
+    /**
+     * Every SwaggerSpringMvcPlugin bean is picked up by the swagger-mvc
+     * framework - allowing for multiple swagger groups i.e. same code base
+     * multiple swagger resource listings.
+     */
+    @Bean
+    public SwaggerSpringMvcPlugin customImplementation()
+    {
+        return new SwaggerSpringMvcPlugin(this.springSwaggerConfig)
+                .apiInfo(apiInfo())
+                .includePatterns(".*");
+    }
+
+    private ApiInfo apiInfo()
+    {
+        return new ApiInfo(
+                "ZHB",
+                "ZHB",
+                "开发者: Example",
+                "example@example.com",
+                "MIT License",
+                "/LICENSE");
+    }
+}
