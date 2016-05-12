b517ddaff737c8b41e43abcda2523f0a02672608
ligm@zhuhui8.com <1QAZ2wsx>
2016-05-12 13:38:59 +0800

    添加oms登陆校验

--- src/main/java/com/zhuhuibao/common/util/ShiroUtil.java
+++ src/main/java/com/zhuhuibao/common/util/ShiroUtil.java
@@ -1,11 +1,13 @@
 package com.zhuhuibao.common.util;
 
-import com.zhuhuibao.shiro.realm.ShiroRealm;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.zhuhuibao.shiro.realm.OMSRealm;
+import com.zhuhuibao.shiro.realm.ShiroRealm;
 
 /**
  * Created by Administrator on 2016/4/13 0013.
@@ -23,6 +25,30 @@ public class ShiroUtil {
                 if(principal != null)
                 {
                     createID = principal.getId();
+                }
+            }
+        }
+        catch(Exception e)
+        {
+            log.error("get seesion user info error!",e);
+        }
+        return createID;
+    }
+    /**
+     * 获取oms用户登陆ID
+     * @return
+     */
+    public static Long  getOmsCreateID()
+    {
+        Long createID = null;
+        try {
+            Subject currentUser = SecurityUtils.getSubject();
+            Session session = currentUser.getSession(false);
+            if (session != null) {
+            	OMSRealm.ShiroOmsUser principal = (OMSRealm.ShiroOmsUser) session.getAttribute("oms");
+                if(principal != null)
+                {
+                    createID = Long.valueOf(principal.getId()+"");
                 }
             }
         }
