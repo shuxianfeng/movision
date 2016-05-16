a5335e44cef6048774abdbede89c3be92fa31a7d
firstcui <1193152880@qq.com>
2016-05-16 15:14:17 +0800

    u

--- src/main/java/com/zhuhuibao/mybatis/memCenter/entity/AskPrice.java
+++ src/main/java/com/zhuhuibao/mybatis/memCenter/entity/AskPrice.java
@@ -1,58 +1,85 @@
 package com.zhuhuibao.mybatis.memCenter.entity;
 
+import com.wordnik.swagger.annotations.ApiModelProperty;
+
 import java.util.Date;
 
 public class AskPrice {
     private Long id;
 
+    @ApiModelProperty(value="发布人ID")
     private String createid;
 
+    @ApiModelProperty(value="询价标题")
     private String title;
 
+    @ApiModelProperty(value="状态 1：询价中，2：结束")
     private Byte status;
 
+    @ApiModelProperty(value="询价类型   1：公开询价，2：定向询价")
     private Byte type;
 
+    @ApiModelProperty(value="产品询价保存的内容")
     private String content;
 
+    @ApiModelProperty(value="询价单的url")
     private String billurl;
 
+    @ApiModelProperty(value="询价发布时间")
     private String publishTime;
 
+    @ApiModelProperty(value="询价截止时间")
     private String endTime;
 
+    @ApiModelProperty(value="交货省")
     private String provinceCode;
 
+    @ApiModelProperty(value="交货市")
     private String cityCode;
 
+    @ApiModelProperty(value="交货区")
     private String areaCode;
 
+    @ApiModelProperty(value="一级分类ID")
     private String fcateid;
 
+    @ApiModelProperty(value="品牌ID")
     private String brandid;
 
+    @ApiModelProperty(value="品牌名称")
     private String brandName;
 
+    @ApiModelProperty(value="产品ID")
     private String productid;
 
+    @ApiModelProperty(value="产品/服务名称")
     private String productName;
 
+    @ApiModelProperty(value="供应商ID。多个用逗号隔开。")
     private String supplierid;
 
+    @ApiModelProperty(value="是否含税 0:不含税，1：含税")
     private String isTax;
 
+    @ApiModelProperty(value="备货周期")
     private Integer period;
 
+    @ApiModelProperty(value="说明")
     private String description;
 
+    @ApiModelProperty(value="1：显示我的联系方式，0：不显示")
     private Boolean isShow;
 
+    @ApiModelProperty(value="公司名称")
     private String companyName;
 
+    @ApiModelProperty(value="公司联系人")
     private String linkMan;
 
+    @ApiModelProperty(value="联系电话")
     private String telephone;
 
+    @ApiModelProperty(value="电子邮箱")
     private String email;
     
     public Long getId() {
