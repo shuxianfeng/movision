ca0fb77cf033565482ebd13d420cd3cbd2822757
penglong <19630759@qq.com>
2016-05-17 17:01:55 +0800

    项目信息

---
+++ src/main/java/com/zhuhuibao/common/constant/cccConstant.java
@@ -0,0 +1,24 @@
+package com.zhuhuibao.common.constant;
+
+/**
+ * 项目信息常量类
+ * @author Administrator
+ * @version 2016/5/17 0017
+ */
+public interface cccConstant {
+
+    /**
+     * 项目详情隐藏符号星号
+     */
+    String PROJECT_HIDDEN_STAR_TEN = "**********";
+
+    /**
+     * 项目详情联系人隐藏星号
+     */
+    String PROJECT_HIDDEN_LINKMAN_STAR_THREE = "***";
+
+    /**
+     * 项目详情手机隐藏星号
+     */
+    String PROJECT_HIDDEN_MOBILE_STAR_FOUR = "****";
+}
