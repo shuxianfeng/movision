a36e4c5a2821e3bd47e35225569324b1558f7b91
jiangliangzhi <jianglz@zhuhui8.com>
2016-05-19 13:46:58 +0800

    1

---
+++ src/main/java/com/zhuhuibao/web/ConstantController.java
@@ -0,0 +1,15 @@
+package com.zhuhuibao.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jianglz
+ * @version 2016-05-19
+ */
+@RestController
+public class ConstantController {
+    private static final Logger log = LoggerFactory.getLogger(AuthenticationController.class);
+
+}
