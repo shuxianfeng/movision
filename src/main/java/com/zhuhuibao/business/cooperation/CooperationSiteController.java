6c4273a11f3444491be4b3a905cfa3201dd05a7e
firstcui <1193152880@qq.com>
2016-05-25 10:09:27 +0800

    发布协会动态

--- src/main/java/com/zhuhuibao/business/cooperation/CooperationSiteController.java
+++ src/main/java/com/zhuhuibao/business/cooperation/CooperationSiteController.java
@@ -23,7 +23,7 @@ import java.util.Map;
  * Created by cxx on 2016/5/4 0004.
  */
 @RestController
-@RequestMapping("/rest/cooperation")
+@RequestMapping("/rest/cooperationSite")
 public class CooperationSiteController {
     private static final Logger log = LoggerFactory.getLogger(CooperationSiteController.class);
 
@@ -86,8 +86,8 @@ public class CooperationSiteController {
      * 威客信息詳情
      */
     @ApiOperation(value="威客信息詳情",notes="威客信息詳情",response = Cooperation.class)
-    @RequestMapping(value = "queryCooperationInfoById", method = RequestMethod.GET)
-    public Response queryCooperationInfo(@RequestParam String id)  {
+    @RequestMapping(value = "cooperationInfo", method = RequestMethod.GET)
+    public Response cooperationInfo(@RequestParam String id)  {
         Response response = new Response();
         Cooperation cooperation = cooperationService.queryCooperationInfoById(id);
         response.setData(cooperation);
