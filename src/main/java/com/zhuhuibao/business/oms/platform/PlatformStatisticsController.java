203885d3a16b580cf2b3d0324e2cb8df2181c067
ligm@zhuhui8.com <1QAZ2wsx>
2016-06-14 14:12:30 +0800

    后天统计模块代码

---
+++ src/main/java/com/zhuhuibao/business/oms/platform/PlatformStatisticsController.java
@@ -0,0 +1,49 @@
+package com.zhuhuibao.business.oms.platform;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
+import com.zhuhuibao.common.Response;
+import com.zhuhuibao.mybatis.oms.entity.PlatformStatistics;
+import com.zhuhuibao.mybatis.oms.service.PlatformStatService;
+import com.zhuhuibao.utils.pagination.model.Paging;
+import com.zhuhuibao.utils.pagination.util.StringUtils;
+
+/**
+ * 统计接口
+ * Created by gmli on 2016/6/13 0004.
+ */
+@RestController
+@RequestMapping("/rest/stat/oms")
+public class PlatformStatisticsController {
+	 private static final Logger log = LoggerFactory.getLogger(PlatformStatisticsController.class);
+	 @Autowired
+	 private PlatformStatService platformStatService;
+	 
+	  /**
+	     * 平台信息统计
+	     * @author gmli
+	     * @since 2016.5.26 
+	     */
+	    @ApiOperation(value="平台信息统计",notes="平台信息统计",response = Response.class)
+	    @RequestMapping(value = "sel_platform_stat", method = RequestMethod.GET)
+	    public Response queryPlatformStatistics() {
+	    	  Response Response = new Response(); 
+	          //查询
+	    	  Map<String,List<PlatformStatistics>> platformStatisticsList = platformStatService.findAllPlatformStat();
+	        
+	          Response.setData(platformStatisticsList);
+	          return Response;
+	    }
+}
---
+++ src/main/java/com/zhuhuibao/mybatis/oms/service/PlatformStatService.java
@@ -0,0 +1,70 @@
+package com.zhuhuibao.mybatis.oms.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.zhuhuibao.mybatis.oms.entity.PlatformStatistics;
+import com.zhuhuibao.mybatis.oms.mapper.PlatformStatMapper;
+
+/**
+ * 
+ * @author gmli 2016.06.13
+ *
+ */
+ @Service
+ @Transactional
+public class PlatformStatService {
+	 private final static Logger log = LoggerFactory.getLogger(TenderTonedService.class);
+
+    @Autowired
+    private PlatformStatMapper platformStatMapper;
+    /**
+	 * 平台统计信息查询
+	 * 
+	 * @author gmli
+	 */
+	public  Map<String,List<PlatformStatistics>> findAllPlatformStat() {
+		 List<PlatformStatistics> statList=null;
+		 //处理查询统计分类
+		 Map<String,List<PlatformStatistics>> resultStatMap=new HashMap();
+		 List<PlatformStatistics> statMap=new ArrayList<PlatformStatistics>();
+		 
+		try{
+			statList= platformStatMapper.findAllPlatformStatistics() ;
+			if(statList!=null&&statList.size()>0)
+			{
+				for(int i=0;i<statList.size();i++)
+				{
+					 
+						
+						if(resultStatMap.get("resultStat"+statList.get(i).getStat_type())!=null)
+						{
+							statMap.add(statList.get(i));
+						    resultStatMap.put("resultStat"+statList.get(i).getStat_type(), statMap);
+						}else{
+							
+							statMap=new ArrayList<PlatformStatistics>();
+							statMap.add(statList.get(i));
+							resultStatMap.put("resultStat"+statList.get(i).getStat_type(), statMap);
+						}
+					 
+				}
+			}
+		} catch (Exception e) {
+
+			log.error(e.getMessage());
+			e.printStackTrace();
+			throw e;
+		}
+ 
+		return resultStatMap;
+	}
+}
