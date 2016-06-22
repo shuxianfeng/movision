a60a534aa762ba89bbdb8b1a7e01bd0150094caa
tongxinglong <tongxl@zhuhui8.com>
2016-06-22 14:09:12 +0800

    筑慧币

--- src/main/java/com/zhuhuibao/common/util/ShiroUtil.java
+++ src/main/java/com/zhuhuibao/common/util/ShiroUtil.java
@@ -12,71 +12,84 @@ import org.slf4j.LoggerFactory;
 import com.zhuhuibao.shiro.realm.OMSRealm;
 import com.zhuhuibao.shiro.realm.ShiroRealm;
 
-
 public class ShiroUtil {
-    private static Logger log = LoggerFactory.getLogger(ShiroUtil.class);
+	private static Logger log = LoggerFactory.getLogger(ShiroUtil.class);
 
-    public static Long getCreateID() {
-        Long createID = null;
-        try {
-            Subject currentUser = SecurityUtils.getSubject();
-            Session session = currentUser.getSession(false);
-            if (session != null) {
-                ShiroRealm.ShiroUser principal = (ShiroRealm.ShiroUser) session.getAttribute("member");
-                if (principal != null) {
-                    createID = principal.getId();
-                }
-            }
-        } catch (Exception e) {
-            log.error("get seesion user info error!", e);
-            throw new AuthException(MsgCodeConstant.un_login,
-                    MsgPropertiesUtils.getValue(String.valueOf(MsgCodeConstant.un_login)));
+	public static ShiroRealm.ShiroUser getMember() {
+		ShiroRealm.ShiroUser member = null;
+		try {
+			Subject currentUser = SecurityUtils.getSubject();
+			Session session = currentUser.getSession(false);
+			if (session != null) {
+				member = (ShiroRealm.ShiroUser) session.getAttribute("member");
+			}
+		} catch (Exception e) {
+			log.error("get seesion user info error!", e);
+			throw new AuthException(MsgCodeConstant.un_login, MsgPropertiesUtils.getValue(String.valueOf(MsgCodeConstant.un_login)));
 
-        }
-        return createID;
-    }
+		}
+		return member;
 
-    public static Long getCompanyID() {
-        Long companyId = null;
-        try {
-            Subject currentUser = SecurityUtils.getSubject();
-            Session session = currentUser.getSession(false);
-            if (session != null) {
-                ShiroRealm.ShiroUser principal = (ShiroRealm.ShiroUser) session.getAttribute("member");
-                if (principal != null) {
-                    companyId = principal.getCompanyId();
-                }
-            }
-        } catch (Exception e) {
-            log.error("get seesion user info error!", e);
-            throw new AuthException(MsgCodeConstant.un_login,
-                    MsgPropertiesUtils.getValue(String.valueOf(MsgCodeConstant.un_login)));
+	}
 
-        }
-        return companyId;
-    }
+	public static Long getCreateID() {
+		Long createID = null;
+		try {
+			Subject currentUser = SecurityUtils.getSubject();
+			Session session = currentUser.getSession(false);
+			if (session != null) {
+				ShiroRealm.ShiroUser principal = (ShiroRealm.ShiroUser) session.getAttribute("member");
+				if (principal != null) {
+					createID = principal.getId();
+				}
+			}
+		} catch (Exception e) {
+			log.error("get seesion user info error!", e);
+			throw new AuthException(MsgCodeConstant.un_login, MsgPropertiesUtils.getValue(String.valueOf(MsgCodeConstant.un_login)));
 
-    /**
-     * 获取oms用户登陆ID
-     *
-     * @return
-     */
-    public static Long getOmsCreateID() {
-        Long createID = null;
-        try {
-            Subject currentUser = SecurityUtils.getSubject();
-            Session session = currentUser.getSession(false);
-            if (session != null) {
-                OMSRealm.ShiroOmsUser principal = (OMSRealm.ShiroOmsUser) session.getAttribute("oms");
-                if (principal != null) {
-                    createID = Long.valueOf(principal.getId() + "");
-                }
-            }
-        } catch (Exception e) {
-            log.error("get seesion user info error!", e);
-            throw new AuthException(MsgCodeConstant.un_login,
-                    MsgPropertiesUtils.getValue(String.valueOf(MsgCodeConstant.un_login)));
-        }
-        return createID;
-    }
+		}
+		return createID;
+	}
+
+	public static Long getCompanyID() {
+		Long companyId = null;
+		try {
+			Subject currentUser = SecurityUtils.getSubject();
+			Session session = currentUser.getSession(false);
+			if (session != null) {
+				ShiroRealm.ShiroUser principal = (ShiroRealm.ShiroUser) session.getAttribute("member");
+				if (principal != null) {
+					companyId = principal.getCompanyId();
+				}
+			}
+		} catch (Exception e) {
+			log.error("get seesion user info error!", e);
+			throw new AuthException(MsgCodeConstant.un_login, MsgPropertiesUtils.getValue(String.valueOf(MsgCodeConstant.un_login)));
+
+		}
+		return companyId;
+	}
+
+	/**
+	 * 获取oms用户登陆ID
+	 *
+	 * @return
+	 */
+	public static Long getOmsCreateID() {
+		Long createID = null;
+		try {
+			Subject currentUser = SecurityUtils.getSubject();
+			Session session = currentUser.getSession(false);
+			if (session != null) {
+				OMSRealm.ShiroOmsUser principal = (OMSRealm.ShiroOmsUser) session.getAttribute("oms");
+				if (principal != null) {
+					createID = Long.valueOf(principal.getId() + "");
+				}
+			}
+		} catch (Exception e) {
+			log.error("get seesion user info error!", e);
+			throw new AuthException(MsgCodeConstant.un_login, MsgPropertiesUtils.getValue(String.valueOf(MsgCodeConstant.un_login)));
+		}
+		return createID;
+	}
 }
