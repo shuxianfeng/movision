49db9ba853c03a48350b43cb0398e2e68f53b48f
jianglz <236881275@qq.com>
2016-06-27 17:13:06 +0800

    Merge branch 'master' of http://139.196.189.100:6001/server/zhuhuibao
    # Conflicts:
    #	src/main/java/com/zhuhuibao/common/pojo/AuthcMember.java
    #	src/main/java/com/zhuhuibao/mybatis/memberReg/service/MemberRegService.java
    #	src/main/java/com/zhuhuibao/shiro/realm/ShiroRealm.java
    #	src/main/java/com/zhuhuibao/web/AuthenticationController.java
    #	src/main/resources/mapper/member/MemberRegMapper.xml
    #	src/main/resources/spring/application.properties
    #	src/main/resources/web/config.properties
    #	src/main/resources/web/pay.properties

--- src/main/java/com/zhuhuibao/utils/RandomFileNamePolicy.java
+++ src/main/java/com/zhuhuibao/utils/RandomFileNamePolicy.java
@@ -1,6 +1,7 @@
 package com.zhuhuibao.utils;
 
 import com.oreilly.servlet.multipart.FileRenamePolicy;
+import com.zhuhuibao.utils.file.FileUtil;
 
 import java.io.File;
 import java.util.Date;
@@ -10,23 +11,7 @@ import java.util.Date;
  */
 public class RandomFileNamePolicy implements FileRenamePolicy {
     public File rename(File file) {
-        /*
-        * 假设，我们上传了一个同名文件，文件名为【abcd.ccc】
-        */
-        int index = file.getName().lastIndexOf("."); //获取文件名中【.】的下标
-        String body = file.getName().substring(0,index);//文件名
-        String postfix = ""; //表示文件名的后缀，即【.ccc】
-        String timer = ""; //代表当前系统时间的数字
 
-        //如果该文件的名字中，没有【.】的话，那么lastIndexOf(".")将返回-1
-        if (index != -1) {
-            timer = new Date().getTime() + ""; //在文件的名字前面，添加的表示当前系统时间的数字
-            postfix = file.getName().substring(index); //获取到文件名当中的【.ccc】
-        } else {
-            timer = new Date().getTime() + "";
-            postfix = ""; //如果lastIndexOf(".")返回-1，说明该文件名中没有【.】即没有后缀
-        }
-        String newName = body + timer + postfix; //构造新的文件名
-        return new File(file.getParent(), newName); //返回重命名后的文件
+        return FileUtil.renameFile(file);
     }
 }
