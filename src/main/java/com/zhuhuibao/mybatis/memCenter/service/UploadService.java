15798cde07b945b4f538524f1da203dda16e00bf
jianglz <236881275@qq.com>
2016-06-28 11:56:49 +0800

    Merge branch 'master' of http://139.196.189.100:6001/server/zhuhuibao
    # Conflicts:
    #	src/main/java/com/zhuhuibao/common/pojo/AuthcMember.java
    #	src/main/java/com/zhuhuibao/mybatis/memberReg/service/MemberRegService.java
    #	src/main/java/com/zhuhuibao/shiro/realm/ShiroRealm.java
    #	src/main/java/com/zhuhuibao/web/AuthenticationController.java
    #	src/main/resources/mapper/member/MemberRegMapper.xml
    #	src/main/resources/spring/application.properties
    #	src/main/resources/web/config.properties
    #	src/main/resources/web/pay.properties

--- src/main/java/com/zhuhuibao/mybatis/memCenter/service/UploadService.java
+++ src/main/java/com/zhuhuibao/mybatis/memCenter/service/UploadService.java
@@ -35,102 +35,23 @@ public class UploadService {
     @Autowired
     ApiConstants ApiConstants;
 
-    public Map<String, String> upload(HttpServletRequest req, String type, String chann) {
-        Map<String, String> result = new HashMap<>();
 
-        try {
-            String saveDirectory;
-            int maxPostSize;
-            String host = PropertiesUtils.getValue("host.ip");
-//            String fileName = FileUtil.renameFile(file.getOriginalFilename());
-            String data = "";
-            switch (type) {
-                case "img":
-                    if (chann != null) {
-                        saveDirectory = ApiConstants.getUploadDir() + "/" + chann + "/img";
-                        maxPostSize = ApiConstants.getUploadPicMaxPostSize();
-                        data = host + "/upload/" + chann + "/img/";
-                    } else {
-                        saveDirectory = ApiConstants.getUploadDir();
-                        maxPostSize = ApiConstants.getUploadPicMaxPostSize();
-                        data = host + "/upload/";
-                    }
-
-                    break;
-                case "doc":
-                    if (chann != null) {
-                        saveDirectory = ApiConstants.getUploadDoc() + "/" + chann + "/doc";
-                        maxPostSize = ApiConstants.getUploadDocMaxPostSize();
-                        if (chann.equals("tech")) {
-                            maxPostSize = ApiConstants.getUploadTechMaxPostSize();
-                        }
-                    } else {
-                        saveDirectory = ApiConstants.getUploadDoc();
-                        maxPostSize = ApiConstants.getUploadDocMaxPostSize();
-                    }
-//                    data = fileName;
-                    break;
-                default:
-                    log.error("上传类型不支持");
-                    throw new BusinessException(MsgCodeConstant.SYSTEM_ERROR, "上传类型不支持");
-            }
-
-
-            //目录不存在则创建
-            File dir = new File(saveDirectory);
-            if (!dir.exists() && !dir.isDirectory()) {
-                dir.mkdirs();
-                log.info("mk dir susscess dirName = " + saveDirectory);
-            }
-
-//            long size = file.getSize();
-//            if (size > maxPostSize) {
-//                throw new BusinessException(MsgCodeConstant.SYSTEM_ERROR, "文件大小超过限制");
-//            }
-//
-//
-//            File upfile = new File(saveDirectory + "/" + fileName);
-//            file.transferTo(upfile);
-            //指定所上传的文件命名规则
-            RandomFileNamePolicy rfnp = new RandomFileNamePolicy();
-            //完成文件上传
-            MultipartRequest multi = new MultipartRequest(req, saveDirectory, maxPostSize, "UTF-8", rfnp);
-
-            Enumeration fileNames = multi.getFileNames();
-            String lastFileName  = "";
-            while (fileNames.hasMoreElements()) {
-                String fileName = (String) fileNames.nextElement();
-                if (null != multi.getFile(fileName)) {
-                    lastFileName = multi.getFilesystemName(fileName);
-                }
-            }
-
-            if("img".equals(type)){
-               data = data + lastFileName;
-            }else if("doc".equals(type)){
-                data = lastFileName;
-            }
-
-            result.put("status", "success");
-            result.put("data", data);
-
-        } catch (Exception e) {
-            log.error("upload error!", e);
-
-            result.put("status", "fail");
-
-        }
-        return result;
-    }
-
-
+    /**
+     * 图片|文件上传 (测试环境) commons-upload
+     * @param file
+     * @param type
+     * @param chann
+     * @return
+     */
     public Map<String, String> upload(MultipartFile file, String type, String chann) {
         Map<String, String> result = new HashMap<>();
 
         try {
             String saveDirectory;
             int maxPostSize;
-            String host = PropertiesUtils.getValue("host.ip");
+            String imgDomain = PropertiesUtils.getValue("img.domain ");
+//            String docDomain = PropertiesUtils.getValue("doc.domain");
+
             String fileName = FileUtil.renameFile(file.getOriginalFilename());
             String data;
             switch (type) {
@@ -138,23 +59,23 @@ public class UploadService {
                     if (chann != null) {
                         saveDirectory = ApiConstants.getUploadDir() + "/" + chann + "/img";
                         maxPostSize = ApiConstants.getUploadPicMaxPostSize();
-                        data = host + "/upload/" + chann + "/img/" + fileName;
+                        data = imgDomain + "/upload/" + chann + "/img/" + fileName;
                     } else {
                         saveDirectory = ApiConstants.getUploadDir();
                         maxPostSize = ApiConstants.getUploadPicMaxPostSize();
-                        data = host + "/upload/" + fileName;
+                        data = imgDomain + "/upload/" + fileName;
                     }
 
                     break;
                 case "doc":
                     if (chann != null) {
-                        saveDirectory = ApiConstants.getUploadDoc() + "/" + chann + "/doc";
+                        saveDirectory = ApiConstants.getUploadDir() + "/" + chann + "/doc";
                         maxPostSize = ApiConstants.getUploadDocMaxPostSize();
                         if (chann.equals("tech")) {
                             maxPostSize = ApiConstants.getUploadTechMaxPostSize();
                         }
                     } else {
-                        saveDirectory = ApiConstants.getUploadDoc();
+                        saveDirectory = ApiConstants.getUploadDir();
                         maxPostSize = ApiConstants.getUploadDocMaxPostSize();
                     }
                     data = fileName;
@@ -196,9 +117,10 @@ public class UploadService {
 
 
     /**
-     * cos插件
+     * cos插件 {废弃}
      * 上传图片，返回url
      */
+    @Deprecated
     public String upload(HttpServletRequest req, String type) throws IOException {
         //指定所上传的文件，上传成功后，在服务器的保存位置
         String url = "";
