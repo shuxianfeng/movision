7e38bdb09c35d8ca2dacd396b050dec4ee463dd2
firstcui <1193152880@qq.com>
2016-06-28 13:46:49 +0800

    u

--- src/main/java/com/zhuhuibao/business/oms/member/MemberOmsController.java
+++ src/main/java/com/zhuhuibao/business/oms/member/MemberOmsController.java
@@ -7,17 +7,12 @@ import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 import com.wordnik.swagger.annotations.ApiParam;
 import com.zhuhuibao.common.Response;
-import com.zhuhuibao.common.constant.Constants;
 import com.zhuhuibao.common.pojo.OmsMemBean;
-import com.zhuhuibao.common.util.ShiroUtil;
 import com.zhuhuibao.mybatis.memCenter.entity.CertificateRecord;
 import com.zhuhuibao.mybatis.memCenter.entity.Identity;
 import com.zhuhuibao.mybatis.memCenter.entity.Member;
 import com.zhuhuibao.mybatis.memCenter.service.MemberService;
 import com.zhuhuibao.mybatis.oms.service.OmsMemService;
-import com.zhuhuibao.mybatis.sitemail.entity.MessageText;
-import com.zhuhuibao.mybatis.sitemail.service.SiteMailService;
-import com.zhuhuibao.shiro.realm.OMSRealm;
 import com.zhuhuibao.utils.pagination.model.Paging;
 import com.zhuhuibao.utils.pagination.util.StringUtils;
 import org.slf4j.Logger;
@@ -31,7 +26,7 @@ import org.springframework.web.bind.annotation.*;
  *
  */
 @RestController
-@RequestMapping()
+@RequestMapping("/rest/member/oms/base/")
 @Api(value = "MemberOmsController",description = "运营平台会员管理")
 public class MemberOmsController {
 	
@@ -43,13 +38,10 @@ public class MemberOmsController {
 	@Autowired
 	private MemberService memberService;
 
-	/**
-	 * 查询全部会员（分页）
-	 * @param member
-	 * @throws IOException
-	 */
-	@RequestMapping(value={"/rest/oms/getAllMemInfo","/rest/member/oms/base/sel_allMember"},method = RequestMethod.GET)
-	public Response getAllMemInfo(OmsMemBean member, String pageNo, String pageSize) throws IOException
+	@ApiOperation(value="查询全部会员（分页）",notes="查询全部会员（分页）",response = Response.class)
+	@RequestMapping(value="sel_allMember",method = RequestMethod.GET)
+	public Response getAllMemInfo(@ModelAttribute OmsMemBean member, @RequestParam(required = false) String pageNo,
+								  @RequestParam(required = false) String pageSize)
 	{
 		if (StringUtils.isEmpty(pageNo)) {
 			pageNo = "1";
@@ -62,13 +54,10 @@ public class MemberOmsController {
 		return result;
 	}
 
-	/**
-	 * 查询全部会员资质（分页）
-	 * @param member
-	 * @throws IOException
-	 */
-	@RequestMapping(value={"/rest/oms/getAllMemCertificate","/rest/member/oms/base/sel_allCertificate"},method = RequestMethod.GET)
-	public Response getAllMemCertificate(OmsMemBean member, String pageNo, String pageSize) throws IOException
+	@ApiOperation(value="查询全部会员资质（分页）",notes="查询全部会员资质（分页）",response = Response.class)
+	@RequestMapping(value="sel_allCertificate",method = RequestMethod.GET)
+	public Response getAllMemCertificate(@ModelAttribute OmsMemBean member, @RequestParam(required = false)String pageNo,
+										 @RequestParam(required = false)String pageSize)
 	{
 		if (StringUtils.isEmpty(pageNo)) {
 			pageNo = "1";
@@ -82,8 +71,9 @@ public class MemberOmsController {
 		return result;
 	}
 
-	@RequestMapping(value="/rest/member/oms/base/sel_certificate",method = RequestMethod.GET)
-	public Response queryCertificateById(String id) throws IOException
+	@RequestMapping(value="sel_certificate",method = RequestMethod.GET)
+	@ApiOperation(value="会员资质查询",notes="会员资质查询",response = Response.class)
+	public Response queryCertificateById(@RequestParam String id) throws IOException
 	{
 		Response response = new Response();
 		CertificateRecord certificateRecord = omsMemService.queryCertificateById(id);
@@ -91,13 +81,7 @@ public class MemberOmsController {
 		return response;
 	}
 
-	/**
-	 * 会员状态审核
-	 * @param member
-	 * @return
-	 * @throws IOException
-	 */
-	@RequestMapping(value = {"/rest/updateStatus","/rest/member/oms/base/upd_mem_status"}, method = RequestMethod.POST)
+	@RequestMapping(value = "upd_mem_status", method = RequestMethod.POST)
 	@ApiOperation(value="会员状态审核",notes="会员状态审核",response = Response.class)
 	public Response updateStatus(@ApiParam(value = "会员信息") @ModelAttribute(value="member")Member member)  {
 		Response result = new Response();
@@ -105,7 +89,7 @@ public class MemberOmsController {
 		return result;
 	}
 
-	@RequestMapping(value = {"/rest/updateCertificate","/rest/member/oms/base/upd_certificate_status"}, method = RequestMethod.POST)
+	@RequestMapping(value = "upd_certificate_status", method = RequestMethod.POST)
 	@ApiOperation(value="资质审核",notes="资质审核",response = Response.class)
 	public Response updateCertificate(@ApiParam(value = "资质信息") @ModelAttribute(value="record")CertificateRecord record)  {
 		Response result = new Response();
@@ -114,7 +98,7 @@ public class MemberOmsController {
 	}
 
 	@ApiOperation(value = "企业身份", notes = "企业身份", response = Response.class)
-	@RequestMapping(value = "/rest/member/oms/base/sel_identityList", method = RequestMethod.GET)
+	@RequestMapping(value = "sel_identityList", method = RequestMethod.GET)
 	public Response identityList()  {
 		Response result = new Response();
 		List<Identity> identity = memberService.findIdentityList();
@@ -123,7 +107,7 @@ public class MemberOmsController {
 	}
 
 	@ApiOperation(value = "查询企业或用户详情", notes = "查询企业或用户详情", response = Response.class)
-	@RequestMapping(value = "/rest/member/oms/base/sel_memberDetail", method = RequestMethod.GET)
+	@RequestMapping(value = "sel_memberDetail", method = RequestMethod.GET)
 	public Response sel_memberDetail(@ApiParam(value = "会员ID") @RequestParam String mem_id)  {
 		Response result = new Response();
 		Member member = memberService.findMemById(mem_id);
