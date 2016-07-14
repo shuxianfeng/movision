f6e58610867944f689116fd501e1bd7d2236fc91
jianglz <236881275@qq.com>
2016-07-14 18:07:55 +0800

    Merge branch 'master' of http://139.196.189.100:6001/server/zhuhuibao
    # Conflicts:
    #	src/main/java/com/zhuhuibao/mybatis/memCenter/service/IndexService.java

--- src/main/java/com/zhuhuibao/business/tech/site/TechCoopController.java
+++ src/main/java/com/zhuhuibao/business/tech/site/TechCoopController.java
@@ -27,6 +27,7 @@ import java.util.Map;
 
 /**
  * 技术合作
+ *
  * @author pl
  * @version 2016/6/8
  */
@@ -46,55 +47,54 @@ public class TechCoopController {
     @Autowired
     PaymentService paymentService;
 
-    @RequestMapping(value="add_tech_cooperation", method = RequestMethod.POST)
-    @ApiOperation(value="发布技术成果",notes = "新增技术技术成果",response = Response.class)
-    public Response insertTechCooperation(@ApiParam(value = "技术合作：技术成果")  @ModelAttribute(value="techCoop")TechCooperation techCoop) throws Exception {
+    @RequestMapping(value = "add_tech_cooperation", method = RequestMethod.POST)
+    @ApiOperation(value = "发布技术成果", notes = "新增技术技术成果", response = Response.class)
+    public Response insertTechCooperation(@ApiParam(value = "技术合作：技术成果") @ModelAttribute(value = "techCoop") TechCooperation techCoop) throws Exception {
         log.info("insert tech cooperation");
         Long createId = ShiroUtil.getCreateID();
-        if(null != createId) {
+        if (null != createId) {
             techCoop.setCreateID(createId);
             int result = techService.insertTechCooperation(techCoop);
-        }else{
+        } else {
             throw new AuthException(MsgCodeConstant.un_login, MsgPropertiesUtils.getValue(String.valueOf(MsgCodeConstant.un_login)));
         }
         return new Response();
     }
 
-    @RequestMapping(value="add_tech_requirement", method = RequestMethod.POST)
-    @ApiOperation(value="发布技术需求",notes = "新增技术需求",response = Response.class)
-    public Response insertTechRequirement(@ApiParam(value = "技术合作：技术需求")  @ModelAttribute(value="techCoop")TechCooperation techCoop) throws Exception {
+    @RequestMapping(value = "add_tech_requirement", method = RequestMethod.POST)
+    @ApiOperation(value = "发布技术需求", notes = "新增技术需求", response = Response.class)
+    public Response insertTechRequirement(@ApiParam(value = "技术合作：技术需求") @ModelAttribute(value = "techCoop") TechCooperation techCoop) throws Exception {
         log.info("insert tech requirement");
         Long createId = ShiroUtil.getCreateID();
-        if(null != createId) {
+        if (null != createId) {
             techCoop.setCreateID(createId);
             int result = techService.insertTechCooperation(techCoop);
-            if(result != 0 ){
+            if (result != 0) {
                 return new Response();
-            } else{
-                throw new BusinessException(MsgCodeConstant.SYSTEM_ERROR,"发布失败");
+            } else {
+                throw new BusinessException(MsgCodeConstant.SYSTEM_ERROR, "发布失败");
             }
-        }else{
+        } else {
             throw new AuthException(MsgCodeConstant.un_login, MsgPropertiesUtils.getValue(String.valueOf(MsgCodeConstant.un_login)));
         }
     }
 
 
-    @RequestMapping(value="sel_tech_cooperation_detail", method = RequestMethod.GET)
-    @ApiOperation(value="查看技术需求详情",notes = "查看技术需求详情",response = Response.class)
-    public Response previewTechCooperation(@ApiParam(value = "技术需求ID")  @RequestParam String techCoopId)
-    {
-        Map<String,Object> map = new HashMap<>();
-        map.put("id",techCoopId);
-        map.put("type",2);
-        Map<String,Object> techCoop = techService.previewTechCooperationDetail(map);
+    @RequestMapping(value = "sel_tech_cooperation_detail", method = RequestMethod.GET)
+    @ApiOperation(value = "查看技术需求详情", notes = "查看技术需求详情", response = Response.class)
+    public Response previewTechCooperation(@ApiParam(value = "技术需求ID") @RequestParam String techCoopId) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", techCoopId);
+        map.put("type", 2);
+        Map<String, Object> techCoop = techService.previewTechCooperationDetail(map);
         techService.updateTechCooperationViews(techCoopId);
         Response response = new Response();
         response.setData(techCoop);
         return response;
     }
 
-    @RequestMapping(value="sel_tech_cooperation", method = RequestMethod.GET)
-    @ApiOperation(value="频道页搜索技术合作(技术成果，技术需求)",notes = "频道页搜索技术合作(技术成果，技术需求)",response = Response.class)
+    @RequestMapping(value = "sel_tech_cooperation", method = RequestMethod.GET)
+    @ApiOperation(value = "频道页搜索技术合作(技术成果，技术需求)", notes = "频道页搜索技术合作(技术成果，技术需求)", response = Response.class)
     public Response findAllTechCooperationPager(@ApiParam(value = "系统分类") @RequestParam(required = false) String systemCategory,
                                                 @ApiParam(value = "应用领域") @RequestParam(required = false) String applicationArea,
                                                 @ApiParam(value = "类型：1成果，2需求") @RequestParam(required = false) String type,
@@ -109,7 +109,7 @@ public class TechCoopController {
             pageSize = "10";
         }
         Paging<Map<String, String>> pager = new Paging<Map<String, String>>(Integer.valueOf(pageNo), Integer.valueOf(pageSize));
-        condition.put("type",type);
+        condition.put("type", type);
         condition.put("systemCategory", systemCategory);
         condition.put("applicationArea", applicationArea);
         condition.put("status", TechConstant.TechCooperationnStatus.AUDITPASS.toString());
@@ -119,15 +119,14 @@ public class TechCoopController {
         return response;
     }
 
-    @RequestMapping(value="sel_views_order", method = RequestMethod.GET)
-    @ApiOperation(value="技术合作的点击排行",notes = "技术合作的点击排行",response = Response.class)
-    public Response findDataViewsOrder()
-    {
+    @RequestMapping(value = "sel_views_order", method = RequestMethod.GET)
+    @ApiOperation(value = "技术合作的点击排行", notes = "技术合作的点击排行", response = Response.class)
+    public Response findDataViewsOrder() {
         Response response = new Response();
-        Map<String,Object> map = new HashMap<String,Object>();
-        map.put("status",TechConstant.TechCooperationnStatus.AUDITPASS.toString());
-        map.put("count",TechConstant.COOP_DOWNLOAD_COUNT_TEN);
-        List<Map<String,String>> techDataList = techService.findCoopViewsOrder(map);
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("status", TechConstant.TechCooperationnStatus.AUDITPASS.toString());
+        map.put("count", TechConstant.COOP_DOWNLOAD_COUNT_TEN);
+        List<Map<String, String>> techDataList = techService.findCoopViewsOrder(map);
         response.setData(techDataList);
         return response;
     }
