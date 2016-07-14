4921b04aa5d163076f37dafb91f5593e7b451308
jianglz <236881275@qq.com>
2016-07-14 15:04:35 +0800

    Merge branch 'master' of http://139.196.189.100:6001/server/zhuhuibao
    # Conflicts:
    #	src/main/java/com/zhuhuibao/mybatis/memCenter/service/IndexService.java

--- src/main/java/com/zhuhuibao/business/system/product/mc/ProductPublishController.java
+++ src/main/java/com/zhuhuibao/business/system/product/mc/ProductPublishController.java
@@ -23,114 +23,108 @@ import com.zhuhuibao.mybatis.product.service.ProductService;
 
 /**
  * 产品控制层
- * @author penglong
  *
+ * @author penglong
  */
 @RestController
-@Api(value = "productSite",description = "产品(前台页面)")
+@Api(value = "productSite", description = "产品(前台页面)")
 public class ProductPublishController {
-	
-	private static final Logger log = LoggerFactory.getLogger(ProductPublishController.class);
-	
-	@Autowired
-	private ProductService productService;
-	@Autowired
-	private ComplainSuggestService suggestService;
-	@Autowired
-    private CategoryMapper categoryMapper;
-	@Autowired
-	private BrandMapper brandMapper;
-	
-	@RequestMapping(value={"/rest/productDetail/queryProductInfoById","/rest/system/site/product/sel_productDetail"}, method = RequestMethod.GET)
-	@ApiOperation(value = "预览产品详情",notes = "预览产品详情",response = Response.class)
-	public Response queryProductInfoById(Long id) throws IOException
-	{
-		Response response = productService.queryProductInfoById(id);
-		productService.updateHit(id);
-		return response;
-	}
-	
-	@RequestMapping(value={"/rest/productDetail/queryPrdDescParamService","/rest/system/site/product/sel_prdDescParam"}, method = RequestMethod.GET)
-	@ApiOperation(value = "产品详情页面预览产品参数",notes = "产品详情页面预览产品参数",response = Response.class)
-	public Response queryPrdDescParamService(Long id) throws IOException
-	{
-		return productService.queryPrdDescParamService(id);
-	}
-	
-	/**
-	 * 品牌详情页面查询二级系统
-	 * @return
-	 * @throws IOException
-	 */
-	@RequestMapping(value = {"/rest/productDetail/querySCateListByBrandId","/rest/system/site/product/sel_sCateListByBrandId"}, method = RequestMethod.GET)
-	@ApiOperation(value = "品牌详情页面查询二级系统",notes = "品牌详情页面查询二级系统",response = Response.class)
-	public Response querySCateListByBrandId(Product product) throws IOException {
-		Response response = new Response();
-		response = productService.querySCateListByBrandId(product);
 
-		return response;
-	}
+    private static final Logger log = LoggerFactory.getLogger(ProductPublishController.class);
 
-	/**
-	 * 品牌详情页面根据二级系统查询产品
-	 * @param product
-	 * @throws IOException
-	 * @throws JsonMappingException
-	 * @throws JsonGenerationException
-	 */
-	@RequestMapping(value={"/rest/productDetail/queryProductInfoBySCategory","/rest/system/site/product/sel_productInfoBySCategory"},method = RequestMethod.GET)
-	@ApiOperation(value = "品牌详情页面根据二级系统查询产品",notes = "品牌详情页面根据二级系统查询产品",response = Response.class)
-	public Response queryProductInfoBySCategory(ProductWithBLOBs product) throws IOException
-	{
-		Response response = new Response();
-		Map<String,Object> productMap = new HashMap<String,Object>();
-		productMap.put("scateid",product.getScateid());
-		productMap.put("brandid",product.getBrandid());
-		productMap.put("status", Constants.product_status_publish);
-		productMap.put("count", Constants.brand_page_product_count);
-		productMap.put("order", "publishTime");
-		response = productService.queryProductInfoBySCategory(productMap);
+    @Autowired
+    private ProductService productService;
 
-		return response;
-	}
+    @RequestMapping(value = {"/rest/productDetail/queryProductInfoById", "/rest/system/site/product/sel_productDetail"}, method = RequestMethod.GET)
+    @ApiOperation(value = "预览产品详情", notes = "预览产品详情", response = Response.class)
+    public Response queryProductInfoById(Long id) throws IOException {
+        Response response = productService.queryProductInfoById(id);
+        productService.updateHit(id);
+        return response;
+    }
 
-	/**
-	 * 产品推荐信息，根据点击率排序
-	 * @throws IOException
-	 * @throws JsonMappingException
-	 * @throws JsonGenerationException
-	 */
-	@RequestMapping(value={"/rest/productDetail/queryRecommendProduct","/rest/system/site/product/sel_recommendProduct"},method = RequestMethod.GET)
-	@ApiOperation(value = "产品推荐信息，根据点击率排序",notes = "产品推荐信息，根据点击率排序",response = Response.class)
-	public Response queryRecommendProduct() throws IOException
-	{
-		Response response = new Response();
-		Map<String,Object> productMap = new HashMap<String,Object>();
-		productMap.put("status", Constants.product_status_publish);
-		productMap.put("count", Constants.recommend_product_count);
-		productMap.put("order", "hit");
-		response = productService.queryRecommendHotProduct(productMap);
+    @RequestMapping(value = {"/rest/productDetail/queryPrdDescParamService", "/rest/system/site/product/sel_prdDescParam"}, method = RequestMethod.GET)
+    @ApiOperation(value = "产品详情页面预览产品参数", notes = "产品详情页面预览产品参数", response = Response.class)
+    public Response queryPrdDescParamService(Long id) throws IOException {
+        Map<String, Object> map = productService.queryPrdDescParamService(id);
+        return new Response(map);
+    }
 
-		return response;
-	}
+    /**
+     * 品牌详情页面查询二级系统
+     *
+     * @return
+     * @throws IOException
+     */
+    @RequestMapping(value = {"/rest/productDetail/querySCateListByBrandId", "/rest/system/site/product/sel_sCateListByBrandId"}, method = RequestMethod.GET)
+    @ApiOperation(value = "品牌详情页面查询二级系统", notes = "品牌详情页面查询二级系统", response = Response.class)
+    public Response querySCateListByBrandId(Product product) throws IOException {
+        Response response = new Response();
+        response = productService.querySCateListByBrandId(product);
 
-	/**
-	 * 查询热点产品
-	 * @throws IOException
-	 * @throws JsonMappingException
-	 * @throws JsonGenerationException
-	 */
-	@RequestMapping(value={"/rest/productDetail/queryHotProduct","/rest/system/site/product/sel_hotProduct"},method = RequestMethod.GET)
-	@ApiOperation(value = "查询热点产品",notes = "查询热点产品",response = Response.class)
-	public Response queryHotProduct() throws IOException
-	{
-		Response response = new Response();
-		Map<String,Object> productMap = new HashMap<String,Object>();
-		productMap.put("status", Constants.product_status_publish);
-		productMap.put("count", Constants.hot_product_count);
-		productMap.put("order", "hit");
-		response = productService.queryRecommendHotProduct(productMap);
+        return response;
+    }
 
-		return response;
-	}
+    /**
+     * 品牌详情页面根据二级系统查询产品
+     *
+     * @param product
+     * @throws IOException
+     * @throws JsonMappingException
+     * @throws JsonGenerationException
+     */
+    @RequestMapping(value = {"/rest/productDetail/queryProductInfoBySCategory", "/rest/system/site/product/sel_productInfoBySCategory"}, method = RequestMethod.GET)
+    @ApiOperation(value = "品牌详情页面根据二级系统查询产品", notes = "品牌详情页面根据二级系统查询产品", response = Response.class)
+    public Response queryProductInfoBySCategory(ProductWithBLOBs product) throws IOException {
+        Response response = new Response();
+        Map<String, Object> productMap = new HashMap<String, Object>();
+        productMap.put("scateid", product.getScateid());
+        productMap.put("brandid", product.getBrandid());
+        productMap.put("status", Constants.product_status_publish);
+        productMap.put("count", Constants.brand_page_product_count);
+        productMap.put("order", "publishTime");
+        response = productService.queryProductInfoBySCategory(productMap);
+
+        return response;
+    }
+
+    /**
+     * 产品推荐信息，根据点击率排序
+     *
+     * @throws IOException
+     * @throws JsonMappingException
+     * @throws JsonGenerationException
+     */
+    @RequestMapping(value = {"/rest/productDetail/queryRecommendProduct", "/rest/system/site/product/sel_recommendProduct"}, method = RequestMethod.GET)
+    @ApiOperation(value = "产品推荐信息，根据点击率排序", notes = "产品推荐信息，根据点击率排序", response = Response.class)
+    public Response queryRecommendProduct() throws IOException {
+        Response response = new Response();
+        Map<String, Object> productMap = new HashMap<String, Object>();
+        productMap.put("status", Constants.product_status_publish);
+        productMap.put("count", Constants.recommend_product_count);
+        productMap.put("order", "hit");
+        response = productService.queryRecommendHotProduct(productMap);
+
+        return response;
+    }
+
+    /**
+     * 查询热点产品
+     *
+     * @throws IOException
+     * @throws JsonMappingException
+     * @throws JsonGenerationException
+     */
+    @RequestMapping(value = {"/rest/productDetail/queryHotProduct", "/rest/system/site/product/sel_hotProduct"}, method = RequestMethod.GET)
+    @ApiOperation(value = "查询热点产品", notes = "查询热点产品", response = Response.class)
+    public Response queryHotProduct() throws IOException {
+        Response response = new Response();
+        Map<String, Object> productMap = new HashMap<String, Object>();
+        productMap.put("status", Constants.product_status_publish);
+        productMap.put("count", Constants.hot_product_count);
+        productMap.put("order", "hit");
+        response = productService.queryRecommendHotProduct(productMap);
+
+        return response;
+    }
 }
