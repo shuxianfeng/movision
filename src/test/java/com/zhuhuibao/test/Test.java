c020e9919da5fda10b78c92f231ea132311dbd29
tongxinglong <tongxl@zhuhui8.com>
2016-07-29 16:08:54 +0800

    测试版本回滚脚本

--- src/test/java/com/zhuhuibao/test/Test.java
+++ src/test/java/com/zhuhuibao/test/Test.java
@@ -1,15 +1,13 @@
 package com.zhuhuibao.test;
 
-
 /**
  * @author jianglz
  * @since 14/11/7.
  */
 public class Test extends BaseSpringContext {
 
-
-    public static void main(String[] args) {
-
-    }
+	public static void main(String[] args) {
+		//test
+	}
 
 }
