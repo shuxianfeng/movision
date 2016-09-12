a3daed97ba28996d3e5176ae90384a0b295b5bc7
zhuangyuhao <zhuangyuhao@zhuhui8.com>
2016-09-12 17:27:32 +0800

    优化注释

--- src/main/java/com/zhuhuibao/mybatis/memCenter/mapper/MemberMapper.java
+++ src/main/java/com/zhuhuibao/mybatis/memCenter/mapper/MemberMapper.java
@@ -11,26 +11,51 @@ import java.util.List;
 import java.util.Map;
 
 public interface MemberMapper {
-    //根据会员ID找到会员信息
+    /**
+     * 根据会员ID找到会员信息
+     * @param id
+     * @return
+     */
     Member findMemById(@Param("id") String id);
 
-    //更新会员信息
+    /**
+     * 更新会员信息
+     * @param member
+     * @return
+     */
     int updateMemInfo(Member member);
 
-    //新建会员
+    /**
+     * 新建会员
+     * @param member
+     * @return
+     */
     int addMember(Member member);
 
-    /* 根据父类ID查询公司下属员工 */
+    /**
+     *  根据父类ID查询公司下属员工 
+     * @param rowBounds
+     * @param map
+     * @return
+     */
     List<Member> findAllByPager(RowBounds rowBounds, Map<String, Object> map);
 
     List<Member> findStaffByParentId(Member member);
 
-    //根据会员账号查询会员
+    /**
+     * 根据会员账号查询会员
+     * @param member
+     * @return
+     */
     Member findMember(Member member);
 
     List<AccountBean> findAgentMember(@Param("account") String account,@Param("type") String type);
 
-    //代理商邮件注册
+    /**
+     * 代理商邮件注册
+     * @param member
+     * @return
+     */
     int agentRegister(Member member);
 
     List<Member> findNewEngineerOrSupplier(@Param("type") String type);
@@ -52,7 +77,10 @@ public interface MemberMapper {
     List<Map<String,String>> findMemberOms(Map<String, Object> map);
 
     List<Map<String,String>> queryEngineerList(Map<String, Object> map);
-
+    /**
+     * 更新企业下所有员工的信息
+     * @param member
+     */
 	void updateSubMemInfo(Member member);
 
     List<Map<String,Object>> findAllInfocheckMemberByPager(RowBounds rowBounds, OmsMemBean member);
