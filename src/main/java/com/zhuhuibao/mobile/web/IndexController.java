ba450188f65c1b17244b2fc65cd5632325a7ec7a
tongxinglong <tongxl@zhuhui8.com>
2016-10-12 09:12:25 +0800

    触屏版首页

---
+++ src/main/java/com/zhuhuibao/mobile/web/IndexController.java
@@ -0,0 +1,13 @@
+package com.zhuhuibao.mobile.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2016/10/11 0011.
+ */
+@RestController
+@RequestMapping("/rest/m/index/site")
+public class IndexController {
+
+}
