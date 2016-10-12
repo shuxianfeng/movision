dca7f3f694fb426f25723f9460be34e0f956fc93
tongxinglong <tongxl@zhuhui8.com>
2016-10-12 17:23:45 +0800

    触屏版会员中心：消息

--- src/main/java/com/zhuhuibao/mobile/web/IndexController.java
+++ src/main/java/com/zhuhuibao/mobile/web/IndexController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Created by Administrator on 2016/10/11 0011.
+ * Created by tongxinglong on 2016/10/11 0011.
  */
 @RestController
 @RequestMapping("/rest/m/index/site")
