e0bf906165fdb496f8f20706f4cf26236d539360
zhuangyuhao <zhuangyuhao@zhuhui8.com>
2016-10-14 17:46:44 +0800

    微信支付

--- src/main/java/com/zhuhuibao/service/wxpay/MobileWxPayService.java
+++ src/main/java/com/zhuhuibao/service/wxpay/MobileWxPayService.java
@@ -1,5 +1,6 @@
 package com.zhuhuibao.service.wxpay;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.SortedMap;
@@ -7,6 +8,7 @@ import java.util.TreeMap;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.jdom.JDOMException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +17,7 @@ import com.mangofactory.swagger.models.Collections;
 import com.zhuhuibao.common.constant.WePayConstant;
 import com.zhuhuibao.utils.HttpClientUtils;
 import com.zhuhuibao.utils.SignUtil;
+import com.zhuhuibao.utils.XmlUtil;
 import com.zhuhuibao.utils.wxpay.WxpayPropertiesLoader;
 
 @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
@@ -31,8 +34,10 @@ public class MobileWxPayService {
 			
 	private static final String MCH_ID = WxpayPropertiesLoader.getPropertyValue("mch_id");
 	
-//	private static final String wx_		
+	private static final String WX_DO_ORDER_URL = WxpayPropertiesLoader.getPropertyValue("wx_do_order_url");
 			
+	private static final String MD5 = "MD5";
+	
 	/**
 	 * 获取微信支付中统一下单接口的传参——openID
 	 * 
@@ -56,6 +61,14 @@ public class MobileWxPayService {
 		return openid;
 	}
 	
+	/**
+	 * 调微信统一下单接口，并处理返回数据
+	 * @param openid
+	 * @param orderid
+	 * @param request
+	 * @param wei_xin_notify_url
+	 * @return
+	 */
 	public Map<String,String> handleOrder(String openid, String orderid, HttpServletRequest request,
 			String wei_xin_notify_url){
 		//生成随机数
@@ -86,12 +99,66 @@ public class MobileWxPayService {
 		signParams.put("sign", sign);
 		signParams.remove("key");//调用统一下单无需key（商户应用密钥）
 		
-		String url = "https://api.mch.weixin.qq.com/pay/unifiedorder";
-		Map<String, String> resultMap = HttpClientUtils.doPostForXml(url, signParams, "UTF-8");
+		Map<String, String> doOrderResultMap = HttpClientUtils.doPostForXml(WX_DO_ORDER_URL, signParams, "UTF-8");
 		
-		System.out.println(resultMap);
+		System.out.println(doOrderResultMap);
 		
-		return null;
+		/**
+		 * 解析结果:返回正确信息
+		 * {result=<xml><return_code><![CDATA[SUCCESS]]></return_code>
+				<return_msg><![CDATA[OK]]></return_msg>
+				<appid><![CDATA[wx5349d63f9159c0bb]]></appid>
+				<mch_id><![CDATA[1393755702]]></mch_id>
+				<nonce_str><![CDATA[ZvYIcsfD0qeey76c]]></nonce_str>
+				<sign><![CDATA[D121BF10B124CFDB65EBCFCFE5C86B2A]]></sign>
+				<result_code><![CDATA[SUCCESS]]></result_code>
+				<prepay_id><![CDATA[wx201610131433133ac4c0257f0243044464]]></prepay_id>
+				<trade_type><![CDATA[JSAPI]]></trade_type>
+				</xml>, status=200}
+		 */
+		Object resultObj = doOrderResultMap.get("result");
+		SortedMap<String, String> jsAPIsignParam = new TreeMap<String, String>();
+		if(null != resultObj){
+			String resultStr = String.valueOf(resultObj);
+			Map map ;
+			try {
+				map = XmlUtil.doXMLParse(resultStr);
+				System.out.println("map>>>"+map.toString());
+				String return_code=(String) map.get("return_code");	//返回状态码
+				String result_code = (String) map.get("result_code");	//业务结果
+				String prepay_id =null;
+				
+				if(return_code.equals("SUCCESS") && result_code.equals("SUCCESS")){
+					prepay_id=(String) map.get("prepay_id");//获取到prepay_id
+				}
+				
+				
+				long currentTimeMillis = System.currentTimeMillis();//生成时间戳
+				long second = currentTimeMillis / 1000L;	//（转换成秒）
+				String seconds = String.valueOf(second).substring(0, 10);	//（截取前10位）
+				
+				jsAPIsignParam.put("appId", APPID);//app_id
+				jsAPIsignParam.put("package", "prepay_id="+prepay_id);//默认sign=WXPay
+				jsAPIsignParam.put("nonceStr", nonce_str);//自定义不重复的长度不长于32位
+				jsAPIsignParam.put("timeStamp",seconds);//北京时间时间戳
+				jsAPIsignParam.put("signType", MD5);
+				
+				String signAgain = SignUtil.createSign("UTF-8", jsAPIsignParam);//再次生成签名
+				
+				jsAPIsignParam.put("paySign", signAgain);
+				
+			} catch (JDOMException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		Map resultMap = new HashMap<>();
+		resultMap.put("doOrderResultMap", doOrderResultMap);
+		resultMap.put("jsAPIsignParam", jsAPIsignParam);
+		return resultMap;
 	}
 	
 	/**
