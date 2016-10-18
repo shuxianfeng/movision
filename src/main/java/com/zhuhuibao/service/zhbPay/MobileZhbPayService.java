2406e13b968ed359fceca0c1378dff305f2ee720
zhuangyuhao <zhuangyuhao@zhuhui8.com>
2016-10-18 11:15:04 +0800

    修改之前的业务逻辑：判断是否是个人或者企业的vip套餐，修改其中的判断条件

--- src/main/java/com/zhuhuibao/service/zhbPay/MobileZhbPayService.java
+++ src/main/java/com/zhuhuibao/service/zhbPay/MobileZhbPayService.java
@@ -1,18 +1,31 @@
-package com.zhuhuibao.business.pay;
+package com.zhuhuibao.service.zhbPay;
+
+import java.awt.Checkbox;
+import java.math.BigDecimal;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.google.gson.Gson;
-import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiOperation;
-import com.wordnik.swagger.annotations.ApiParam;
 import com.zhuhuibao.alipay.util.AlipayPropertiesLoader;
-import com.zhuhuibao.common.Response;
 import com.zhuhuibao.common.constant.MsgCodeConstant;
 import com.zhuhuibao.common.constant.OrderConstants;
 import com.zhuhuibao.common.constant.PayConstants;
 import com.zhuhuibao.common.constant.VipConstant;
 import com.zhuhuibao.common.pojo.CourseOrderReqBean;
-import com.zhuhuibao.common.pojo.ZHBOrderReqBean;
 import com.zhuhuibao.common.pojo.PayReqBean;
+import com.zhuhuibao.common.pojo.ZHBOrderReqBean;
 import com.zhuhuibao.common.util.ShiroUtil;
 import com.zhuhuibao.exception.AuthException;
 import com.zhuhuibao.exception.BusinessException;
@@ -26,34 +39,22 @@ import com.zhuhuibao.utils.IdGenerator;
 import com.zhuhuibao.utils.MsgPropertiesUtils;
 import com.zhuhuibao.utils.ValidateUtils;
 import com.zhuhuibao.utils.pagination.util.StringUtils;
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.session.Session;
-import org.apache.shiro.subject.Subject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.math.BigDecimal;
-import java.util.Map;
 
 /**
- * 订单下单支付
+ * ZHB支付服务
+ * @author zhuangyuhao
+ * @time   2016年10月17日 下午7:27:42
  *
- * @author jianglz
- * @since 16/6/23.
  */
-@RestController
-@RequestMapping("/rest/pay/site")
-@Api(value = "zhbOrderPAY", description = "订单下单支付")
-public class ZhbPayController {
-    private static final Logger log = LoggerFactory.getLogger(ZhbPayController.class);
-
-    private static final String PARTNER = AlipayPropertiesLoader.getPropertyValue("partner");
-
-    @Autowired
+@Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+@Service
+public class MobileZhbPayService {
+	
+	private static final Logger log = LoggerFactory.getLogger(MobileZhbPayService.class);
+	
+	private static final String PARTNER = AlipayPropertiesLoader.getPropertyValue("partner");
+	
+	@Autowired
     ZhpayService zhpayService;
 
     @Autowired
@@ -64,19 +65,21 @@ public class ZhbPayController {
 
     @Autowired
     ZhbService zhbService;
-
-
-    @ApiOperation(value = "筑慧币|VIP购买提交订单", notes = "筑慧币|VIP购买提交订单", response = Response.class)
-    @RequestMapping(value = "do_zhb_order", method = RequestMethod.POST)
-    public Response doZHBOrder(@ApiParam @ModelAttribute ZHBOrderReqBean order) {
-        Gson gson = new Gson();
+	
+    /**
+     * 提交ZHB订单
+     * @param order
+     */
+	public String doZHBOrder(ZHBOrderReqBean order){
+		
+		Gson gson = new Gson();
         String json = gson.toJson(order);
 
         log.info("筑慧币下单页面,请求参数:{}", json);
-        Map paramMap = gson.fromJson(json, Map.class);
+        @SuppressWarnings("unchecked")
+		Map<String, String> paramMap = gson.fromJson(json, Map.class);
 
         checkUserLogin(paramMap);
-
         //根据商品ID查询商品价格
         DictionaryZhbgoods zhbgoods = zhbService.getZhbGoodsById(order.getGoodsId());
         if (zhbgoods == null) {
@@ -99,74 +102,53 @@ public class ZhbPayController {
         //生成订单编号
         String orderNo = IdGenerator.createOrderNo();
         paramMap.put("orderNo", orderNo);
-
         //提交订单
         zhOrderService.createOrder(paramMap);
-        Response response = new Response();
-        response.setData(orderNo);
-        return response;
-    }
-
-    /**
+        
+        return orderNo;
+	}
+	
+	/**
      * 判断用户是否具有改VIP套餐
      *
      * @param goodsType
      * @param value     viplevel
      */
     private void checkVip(String goodsType, String value) {
+    	
         if (goodsType.equals(OrderConstants.GoodsType.VIP.toString())) {
+        	
             Subject currentUser = SecurityUtils.getSubject();
             Session session = currentUser.getSession(false);
             ShiroRealm.ShiroUser user = (ShiroRealm.ShiroUser) session.getAttribute("member");
             String identify = user.getIdentify();
+            
             if (identify.equals("2")) {     //个人
                 boolean suc1 = value.equals(VipConstant.VipLevel.PERSON_GOLD.toString());
                 boolean suc2 = value.equals(VipConstant.VipLevel.PERSON_PLATINUM.toString());
-                if (!suc1 && !suc2) {
+                //如果suc1不是真，并且suc2不是真，则抛异常
+//                if (!(!suc1 || !suc2)) {
+                if(!suc1 && !suc2){	//!false && !false
                     throw new BusinessException(MsgCodeConstant.PARAMS_VALIDATE_ERROR, "个人用户无此VIP套餐");
                 }
             } else {  //企业
                 boolean suc1 = value.equals(VipConstant.VipLevel.ENTERPRISE_GOLD.toString());
                 boolean suc2 = value.equals(VipConstant.VipLevel.ENTERPRISE_PLATINUM.toString());
 
-                if (!suc1 && !suc2) {
+                if(!suc1 && !suc2){
                     throw new BusinessException(MsgCodeConstant.PARAMS_VALIDATE_ERROR, "企业用户无此VIP套餐");
                 }
-
             }
+            
         }
     }
-
-    @ApiOperation(value = "培训课程下单", notes = "培训课程下单", response = Response.class)
-    @RequestMapping(value = "do_course_order", method = RequestMethod.POST)
-    public Response createOrder(@ApiParam @ModelAttribute CourseOrderReqBean order) {
-        Gson gson = new Gson();
-        String json = gson.toJson(order);
-
-        log.info("培训下单页面,请求参数:{}", json);
-        Map paramMap = gson.fromJson(json, Map.class);
-
-        checkUserLogin(paramMap);
-
-        paramMap.put("partner", PARTNER);//partner=seller_id     商家支付宝ID  合作伙伴身份ID 签约账号
-
-        log.debug("调用下单接口......");
-        String orderNo = IdGenerator.createOrderNo();
-        paramMap.put("orderNo", orderNo);
-
-        courseService.createOrder(paramMap);
-
-        Response response = new Response();
-        response.setData(orderNo);
-        return response;
-    }
-
-    /**
+	
+	/**
      * 检查购买用户是否登录
      *
      * @param paramMap
      */
-    private void checkUserLogin(Map paramMap) {
+    private void checkUserLogin(Map<String, String> paramMap) {
         String buyerId = (String) paramMap.get("buyerId");
         if (StringUtils.isEmpty(buyerId)) {
             Long userId = ShiroUtil.getCreateID();
@@ -179,35 +161,50 @@ public class ZhbPayController {
             }
         }
     }
+    
+    /**
+     * 生成订单
+     * @param order
+     * @return
+     */
+    public String createOrder(CourseOrderReqBean order){
+    	
+    	Gson gson = new Gson();
+        String json = gson.toJson(order);
 
-    @ApiOperation(value = "筑慧币单独支付", notes = "筑慧币单独支付")
-    @RequestMapping(value = "do_zhb_pay", method = RequestMethod.POST)
-    public Response doZhbPay(@ApiParam("订单号") @RequestParam String orderNo) {
-        Response response = new Response();
-        try {
-            int result = zhbService.payForOrder(orderNo);
-            int code = result == 1 ? 200 : 400;
-            response.setCode(code);
+        log.info("培训下单页面,请求参数:{}", json);
+        
+        @SuppressWarnings("unchecked")
+		Map<String, String> paramMap = gson.fromJson(json, Map.class);
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new BusinessException(MsgCodeConstant.PAY_ERROR, "支付失败");
-        }
-        return response;
+        checkUserLogin(paramMap);
+
+        paramMap.put("partner", PARTNER);//partner=seller_id     商家支付宝ID  合作伙伴身份ID 签约账号
+
+        log.debug("调用下单接口......");
+        String orderNo = IdGenerator.createOrderNo();
+        paramMap.put("orderNo", orderNo);
+
+        courseService.createOrder(paramMap);
+        
+        return orderNo; 
     }
-
-
-    @ApiOperation(value = "立即支付", notes = "立即支付")
-    @RequestMapping(value = "do_pay", method = RequestMethod.POST)
-    public void doPay(HttpServletRequest request, HttpServletResponse response,
-                      @ApiParam @ModelAttribute PayReqBean pay) throws Exception {
-
-        Gson gson = new Gson();
+    
+    /**
+     * 立即支付
+     * @param request
+     * @param response
+     * @param pay
+     * @throws Exception
+     */
+    public void doPay(HttpServletRequest request, HttpServletResponse response, PayReqBean pay) throws Exception  {
+    	
+    	Gson gson = new Gson();
         String json = gson.toJson(pay);
 
-
         log.info("技术培训支付页面,请求参数:{}", json);
-        Map paramMap = gson.fromJson(json, Map.class);
+        @SuppressWarnings("unchecked")
+		Map<String, String> paramMap = gson.fromJson(json, Map.class);
 
         checkUserLogin(paramMap);
 
@@ -231,6 +228,7 @@ public class ZhbPayController {
             default:
                 throw new BusinessException(MsgCodeConstant.SYSTEM_ERROR, "是否使用筑慧币,传参错误");
         }
-
-    }
+		
+	}
+    
 }
