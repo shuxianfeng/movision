0849c730507d4c7f77974bf6db78a646f1742b55
zhuangyuhao <zhuangyuhao@zhuhui8.com>
2016-10-19 11:59:36 +0800

    微信支付通知回调，加上并发锁

--- src/main/java/com/zhuhuibao/mobile/web/pay/MobileWexinPayController.java
+++ src/main/java/com/zhuhuibao/mobile/web/pay/MobileWexinPayController.java
@@ -22,6 +22,7 @@ import com.aliyuncs.http.HttpRequest;
 import com.taobao.api.domain.BizResult;
 import com.wordnik.swagger.annotations.ApiOperation;
 import com.wordnik.swagger.annotations.ApiParam;
+import com.zhuhuibao.aop.LoginAccess;
 import com.zhuhuibao.common.Response;
 import com.zhuhuibao.common.constant.MsgCodeConstant;
 import com.zhuhuibao.common.constant.PayConstants;
@@ -48,6 +49,7 @@ public class MobileWexinPayController {
 	
 	@ApiOperation(value = "获取调用微信统一下单接口的APPID", notes = "获取调用微信统一下单接口的APPID", response = Response.class)
     @RequestMapping(value = "getAppId", method = RequestMethod.POST)
+	@LoginAccess
 	public Response getAppId(){
 		
 		String appid = WxpayPropertiesLoader.getPropertyValue("app_id");
@@ -58,6 +60,7 @@ public class MobileWexinPayController {
 	
 	@ApiOperation(value = "调用微信统一下单接口,获取JSAPI参数", notes = "调用微信统一下单接口,获取JSAPI参数", response = Response.class)
     @RequestMapping(value = "getWxPayJSAPIParams", method = RequestMethod.POST)
+	@LoginAccess
 	public Response getWxPayJSAPIParams(
 			@ApiParam(value = "请求code") @RequestParam(required = true) String code,
 			@ApiParam(value = "订单编号") @RequestParam(required = true) String orderid,
@@ -74,54 +77,10 @@ public class MobileWexinPayController {
 	}
 	
 	@RequestMapping(value = "getWxPayNotify", method = RequestMethod.POST)
-	public ModelAndView getWxPayNotify(HttpServletRequest request, HttpServletResponse response) throws JDOMException, IOException, ParseException{
-		//交易类型
-		String tradeType = PayConstants.TradeType.PAY.toString();
+	@LoginAccess
+	public ModelAndView getWxPayNotify(HttpServletRequest request) throws JDOMException, IOException, ParseException{
 		
-		ModelAndView modelAndView = new ModelAndView();
-		
-		Map requestParams = request.getParameterMap();
-		if(null == requestParams){
-			throw new BusinessException(MsgCodeConstant.NOTIFY_PARAMS_EMPTY_ERROR, "微信支付结果通用通知接口参数为空");
-		}
-		Object returnObj = requestParams.get("return");
-		if(null == returnObj){
-			throw new BusinessException(MsgCodeConstant.NOTIFY_PARAMS_EMPTY_ERROR, "微信支付结果通用通知接口参数为空");
-		}
-		
-		Map requestMap = new HashMap<>();
-		requestMap = XmlUtil.doXMLParse(String.valueOf(returnObj));
-		if(null == requestMap){
-			throw new BusinessException(MsgCodeConstant.NOTIFY_PARAMS_EMPTY_ERROR, "微信支付结果通用通知接口参数为空");
-		}
-		
-		String return_code = (String)requestParams.get("return_code");
-		
-		if("SUCCESS".equals(return_code)){
-			
-			String orderno = (String)requestMap.get("out_trade_no");
-			//返回成功时业务逻辑处理
-            Map<String, String> resultMap = mobileWxPaySV.tradeSuccessDeal(requestMap,
-                    PayConstants.NotifyType.SYNC.toString(), tradeType);
-            
-            log.info("***同步回调：支付平台回调发起方支付方结果：" + resultMap);
-			if (resultMap != null
-                    && String.valueOf(PayConstants.HTTP_SUCCESS_CODE)
-                    .equals(resultMap.get("statusCode"))) {
-				
-                if ("SUCCESS".equals(resultMap.get("result"))) {
-                	
-                    modelAndView.addObject("return_code", "SUCCESS");
-                    modelAndView.addObject("return_msg", "支付成功");
-                }
-                
-            }
-		}else{
-			modelAndView.addObject("return_code", "FAIL");
-            modelAndView.addObject("return_msg", "支付失败");
-		}
-		
-        return modelAndView; 
+		return mobileWxPaySV.handleWxPayNotify(request);
 	}
 	
 	
