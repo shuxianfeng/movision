579560162d4f027729983a9fc4dc754564fd749e
zhuangyuhao <zhuangyuhao@zhuhui8.com>
2016-10-25 17:10:29 +0800

    添加微信支付查询订单接口

--- src/main/java/com/zhuhuibao/mybatis/order/entity/OrderFlow.java
+++ src/main/java/com/zhuhuibao/mybatis/order/entity/OrderFlow.java
@@ -22,6 +22,16 @@ public class OrderFlow {
     
     private String sign;
     
+    private String transaction_id;
+    
+
+	public String getTransaction_id() {
+		return transaction_id;
+	}
+
+	public void setTransaction_id(String transaction_id) {
+		this.transaction_id = transaction_id;
+	}
 
 	public String getSign() {
 		return sign;
