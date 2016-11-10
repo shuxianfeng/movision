1e3bf5ab799c4c198d449698c4a7fae73cb0c4e7
ligm@zhuhui8.vom <ligm@zhuhui8.vom>
2016-11-10 17:35:24 +0800

    修改bug

--- src/main/java/com/zhuhuibao/alipay/util/AlipayNotify.java
+++ src/main/java/com/zhuhuibao/alipay/util/AlipayNotify.java
@@ -1,6 +1,5 @@
 package com.zhuhuibao.alipay.util;
 
-
 import com.alipay.api.AlipayApiException;
 import com.alipay.api.internal.util.AlipaySignature;
 import com.zhuhuibao.alipay.sign.RSA;
@@ -38,14 +37,15 @@ public class AlipayNotify {
     /**
      * 验证消息是否是支付宝发出的合法消息
      *
-     * @param params 通知返回来的参数数组
+     * @param params
+     *            通知返回来的参数数组
      * @return 验证结果
      */
     public static boolean verify(Map<String, String> params) {
 
-        //判断responsetTxt是否为true，isSign是否为true
-        //responsetTxt的结果不是true，与服务器设置问题、合作身份者ID、notify_id一分钟失效有关
-        //isSign不是true，与安全校验码、请求时的参数格式（如：带自定义参数等）、编码格式有关
+        // 判断responsetTxt是否为true，isSign是否为true
+        // responsetTxt的结果不是true，与服务器设置问题、合作身份者ID、notify_id一分钟失效有关
+        // isSign不是true，与安全校验码、请求时的参数格式（如：带自定义参数等）、编码格式有关
         String responseTxt = "false";
         if (params.get("notify_id") != null) {
             String notify_id = params.get("notify_id");
@@ -57,9 +57,10 @@ public class AlipayNotify {
         }
         boolean isSign = getSignVeryfy(params, sign);
 
-        //写日志记录（若要调试，请取消下面两行注释）
-        //String sWord = "responseTxt=" + responseTxt + "\n isSign=" + isSign + "\n 返回回来的参数：" + AlipayCore.createLinkString(params);
-        //AlipayCore.logResult(sWord);
+        // 写日志记录（若要调试，请取消下面两行注释）
+        // String sWord = "responseTxt=" + responseTxt + "\n isSign=" + isSign +
+        // "\n 返回回来的参数：" + AlipayCore.createLinkString(params);
+        // AlipayCore.logResult(sWord);
 
         return isSign && responseTxt.equals("true");
     }
@@ -67,20 +68,21 @@ public class AlipayNotify {
     /**
      * 根据反馈回来的信息，生成签名结果
      *
-     * @param Params 通知返回来的参数数组
-     * @param sign   比对的签名结果
+     * @param Params
+     *            通知返回来的参数数组
+     * @param sign
+     *            比对的签名结果
      * @return 生成的签名结果
      */
     private static boolean getSignVeryfy(Map<String, String> Params, String sign) {
-        //过滤空值、sign与sign_type参数
+        // 过滤空值、sign与sign_type参数
         Map<String, String> sParaNew = AlipayCore.paraFilter(Params);
-        //获取待签名字符串
+        // 获取待签名字符串
         String preSignStr = AlipayCore.createLinkString(sParaNew);
-        //获得签名验证结果
+        // 获得签名验证结果
         boolean isSign = false;
         if (AlipayPropertiesLoader.getPropertyValue("sign_type").equals("RSA")) {
-            isSign = RSA.verify(preSignStr, sign, AlipayPropertiesLoader.getPropertyValue("alipay_public_key"),
-                    AlipayPropertiesLoader.getPropertyValue("input_charset"));
+            isSign = RSA.verify(preSignStr, sign, AlipayPropertiesLoader.getPropertyValue("alipay_public_key"), AlipayPropertiesLoader.getPropertyValue("input_charset"));
         }
         return isSign;
     }
@@ -88,15 +90,13 @@ public class AlipayNotify {
     /**
      * 获取远程服务器ATN结果,验证返回URL
      *
-     * @param notify_id 通知校验ID
-     * @return 服务器ATN结果
-     * 验证结果集：
-     * invalid命令参数不对 出现这个错误，请检测返回处理中partner和key是否为空
-     * true 返回正确信息
-     * false 请检查防火墙或者是服务器阻止端口问题以及验证时间是否超过一分钟
+     * @param notify_id
+     *            通知校验ID
+     * @return 服务器ATN结果 验证结果集： invalid命令参数不对 出现这个错误，请检测返回处理中partner和key是否为空 true
+     *         返回正确信息 false 请检查防火墙或者是服务器阻止端口问题以及验证时间是否超过一分钟
      */
     private static String verifyResponse(String notify_id) {
-        //获取远程服务器ATN结果，验证是否是支付宝服务器发来的请求
+        // 获取远程服务器ATN结果，验证是否是支付宝服务器发来的请求
 
         String partner = AlipayPropertiesLoader.getPropertyValue("partner");
         String veryfy_url = HTTPS_VERIFY_URL + "partner=" + partner + "&notify_id=" + notify_id;
@@ -107,12 +107,10 @@ public class AlipayNotify {
     /**
      * 获取远程服务器ATN结果
      *
-     * @param urlvalue 指定URL路径地址
-     * @return 服务器ATN结果
-     * 验证结果集：
-     * invalid命令参数不对 出现这个错误，请检测返回处理中partner和key是否为空
-     * true 返回正确信息
-     * false 请检查防火墙或者是服务器阻止端口问题以及验证时间是否超过一分钟
+     * @param urlvalue
+     *            指定URL路径地址
+     * @return 服务器ATN结果 验证结果集： invalid命令参数不对 出现这个错误，请检测返回处理中partner和key是否为空 true
+     *         返回正确信息 false 请检查防火墙或者是服务器阻止端口问题以及验证时间是否超过一分钟
      */
     private static String checkUrl(String urlvalue) {
         String inputLine = "";
@@ -120,8 +118,7 @@ public class AlipayNotify {
         try {
             URL url = new URL(urlvalue);
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection
-                    .getInputStream()));
+            BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
             inputLine = in.readLine().toString();
         } catch (Exception e) {
             e.printStackTrace();
@@ -147,14 +144,15 @@ public class AlipayNotify {
         if (params.get("sign") != null) {
             sign = params.get("sign");
         }
-        //过滤空值、sign与sign_type参数
+        // 过滤空值、sign与sign_type参数
         Map<String, String> sParaNew = AlipayCore.paraFilter(params);
-        //获取待签名字符串
+        // 获取待签名字符串
         String preSignStr = AlipayCore.createLinkString(sParaNew);
-        //获得签名验证结果
+        // 获得签名验证结果
         boolean isSign = AlipaySignature.rsaCheckContent(preSignStr, sign, AlipayPropertiesLoader.getPropertyValue("h5_alipay_zfb_public_key"),
                 AlipayPropertiesLoader.getPropertyValue("input_charset"));
-        log.debug("responseTxt=" + responseTxt + "\n isSign=" + isSign + "\n 返回回来的参数：" + AlipayCore.createLinkString(params) +"\n preSignStr：" + preSignStr);
+        boolean ifSuc = isSign && responseTxt.equals("true");
+        log.debug("responseTxt=" + responseTxt + "\n isSign=" + isSign + "\n 返回回来的参数：" + AlipayCore.createLinkString(params) + "\n preSignStr：" + preSignStr + "ifSuc:" + ifSuc);
         return isSign && responseTxt.equals("true");
     }
 }
