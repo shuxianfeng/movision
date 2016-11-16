37e84a170ca4e35eaa148d081013a91d8c3f476f
ligm@zhuhui8.vom <ligm@zhuhui8.vom>
2016-11-16 09:12:12 +0800

    筑慧专家和招聘接口开发

---
+++ src/main/java/com/zhuhuibao/mobile/web/mc/MobileExpertController.java
@@ -0,0 +1,16 @@
+package com.zhuhuibao.mobile.web.mc;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 筑慧专家controller
+ * 
+ * @author liyang
+ * @date 2016年11月16日
+ */
+@RestController
+@RequestMapping("/rest/m/expert/mc")
+public class MobileExpertController {
+
+}
---
+++ src/main/java/com/zhuhuibao/mobile/web/mc/MobileJobController.java
@@ -0,0 +1,15 @@
+package com.zhuhuibao.mobile.web.mc;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 求职招聘controller
+ *
+ * @author liyang
+ * @date 2016年11月16日
+ */
+@RestController
+@RequestMapping("/rest/m/job/mc")
+public class MobileJobController {
+}
