1cf3506dd8f78c8698e23562ee176148723bfe9f
ligm@zhuhui8.vom <ligm@zhuhui8.vom>
2016-11-16 10:24:38 +0800

    筑慧专家和招聘接口开发

--- src/main/java/com/zhuhuibao/mobile/web/mc/MobileExpertController.java
+++ src/main/java/com/zhuhuibao/mobile/web/mc/MobileExpertController.java
@@ -7,6 +7,8 @@ import com.zhuhuibao.common.constant.MsgCodeConstant;
 import com.zhuhuibao.exception.AuthException;
 import com.zhuhuibao.service.MobileExpertListService;
 import com.zhuhuibao.utils.MsgPropertiesUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,24 +27,24 @@ import java.util.Map;
 @RequestMapping("/rest/m/expert/mc")
 public class MobileExpertController {
 
+    private static final Logger log = LoggerFactory.getLogger(MobileExpertController.class);
 
     @Autowired
     private MobileExpertListService expertListService;
 
-
-    @RequestMapping(value = "sel_my_looked_expertList", method = RequestMethod.GET)
-    @ApiOperation(value = "移动端查询我查看过的专家列表", notes = "移动端查询我查看过的专家列表", response = Response.class)
+    @RequestMapping(value = "sel_my_looked_expert_list", method = RequestMethod.GET)
+    @ApiOperation(value = "触屏端-筑慧中心-我查看过的专家列表", notes = "触屏端-筑慧中心-我查看过的专家列表", response = Response.class)
     public Response selGreatExpertList(@ApiParam(value = "会员的Id") @RequestParam(required = true) String id) {
         Response response = new Response();
         try {
-               Map<String, Object> map=expertListService.findAllMyLookedExpertListById(id);
-               response.setData(map);
+            Map<String, Object> map = expertListService.findAllMyLookedExpertListById(id);
+            response.setData(map);
         } catch (Exception e) {
-            throw new AuthException(MsgCodeConstant.un_login, MsgPropertiesUtils.getValue(String
-                    .valueOf(MsgCodeConstant.un_login)));
+            log.error("sel_my_looked_expert_list error! ", e);
+            e.printStackTrace();
+            throw new AuthException(MsgCodeConstant.un_login, MsgPropertiesUtils.getValue(String.valueOf(MsgCodeConstant.un_login)));
         }
         return response;
     }
-
 
 }
