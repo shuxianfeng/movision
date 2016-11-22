bb6431847ba308b35ae211095bcc92e312f602f5
ligm@zhuhui8.vom <ligm@zhuhui8.vom>
2016-11-22 16:56:01 +0800

    微信资讯功能开发

--- src/main/java/com/external/controller/WeChatNewsController.java
+++ src/main/java/com/external/controller/WeChatNewsController.java
@@ -10,7 +10,9 @@ import com.zhuhuibao.utils.pagination.model.Paging;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 微信h5资讯信息controller
@@ -25,6 +27,24 @@ public class WeChatNewsController {
     @Autowired
     private WeChatNewsService weChatNewsService;
 
+    @RequestMapping(value = { "site/sel_weChat_news_list" }, method = RequestMethod.GET)
+    @ApiOperation(value = "微信h5资讯信息", notes = "微信h5资讯信息", response = Response.class)
+    public Response selSiteWeChatNewsList(@ApiParam(value = "资讯分类, 1:banner,2:魅力时尚,3:健康人生,4留情岁月,5:成人性趣") @RequestParam(required = false) String type,
+            @ApiParam(value = "页码") @RequestParam(required = false, defaultValue = "1") int pageNo, @ApiParam(value = "每页显示的条数") @RequestParam(required = false, defaultValue = "10") int pageSize)
+            throws Exception {
+        Response response = new Response();
+        Map result = new HashMap();
+        Paging<WeChatNewsForm> pager = new Paging<>(pageNo, pageSize);
+        List<WeChatNewsForm> contentList = weChatNewsService.selWeChatNewsList("", type, "1", pager);
+        pager.result(contentList);
+        result.put("contentList", pager);
+        pager = new Paging<>(1, 3);
+        List<WeChatNewsForm> bannerList = weChatNewsService.selWeChatNewsList("", "1", "1", pager);
+        result.put("bannerList", bannerList);
+        response.setData(result);
+        return response;
+    }
+
     @RequestMapping(value = { "oms/sel_weChat_news_list" }, method = RequestMethod.GET)
     @ApiOperation(value = "微信h5资讯信息", notes = "微信h5资讯信息", response = Response.class)
     public Response selWeChatNewsList(@ApiParam(value = "资讯标题") @RequestParam(required = false) String title,
@@ -33,7 +53,7 @@ public class WeChatNewsController {
             @ApiParam(value = "每页显示的条数") @RequestParam(required = false, defaultValue = "10") int pageSize) throws Exception {
         Response response = new Response();
         Paging<WeChatNewsForm> pager = new Paging<>(pageNo, pageSize);
-        List<WeChatNewsForm>  list = weChatNewsService.selWeChatNewsList(title, type, status, pager);
+        List<WeChatNewsForm> list = weChatNewsService.selWeChatNewsList(title, type, status, pager);
         pager.result(list);
         response.setData(pager);
         return response;
@@ -43,8 +63,8 @@ public class WeChatNewsController {
     @ApiOperation(value = "添加微信h5资讯信息", notes = "添加微信h5资讯信息", response = Response.class)
     public Response addWeChatNews(@ModelAttribute WeChatNews news) {
         weChatNewsService.addWeChatNews(news);
-    return new Response();
-}
+        return new Response();
+    }
 
     @RequestMapping(value = "oms/upd_weChat_news", method = RequestMethod.POST)
     @ApiOperation(value = "更新微信h5资讯信息", notes = "更新微信h5资讯信息", response = Response.class)
