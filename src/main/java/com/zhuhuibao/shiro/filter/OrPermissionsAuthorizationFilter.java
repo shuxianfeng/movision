f98f0935f4dd1c88453147738ab7ff7bf11a561f
ligm@zhuhui8.vom <ligm@zhuhui8.vom>
2016-12-01 19:43:49 +0800

    修改bug

--- src/main/java/com/zhuhuibao/shiro/filter/OrPermissionsAuthorizationFilter.java
+++ src/main/java/com/zhuhuibao/shiro/filter/OrPermissionsAuthorizationFilter.java
@@ -1,6 +1,5 @@
 package com.zhuhuibao.shiro.filter;
 
-import com.zhuhuibao.shiro.realm.ShiroRealm;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.authz.AuthorizationFilter;
 
@@ -14,47 +13,17 @@ import java.io.IOException;
 public class OrPermissionsAuthorizationFilter extends AuthorizationFilter {
 
     @Override
-    protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) throws IOException {
+    protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) throws IOException  {
         Subject subject = getSubject(request, response);
         String[] perms = (String[]) mappedValue;
 
-        if (null == subject.getPrincipal()) {
-            return false;
-        }
-
         for (String perm : perms) {
-            String[] permArr = perm.split(":");
-            if (!isAccessAllowedRealization(permArr, subject)) {
-                return false;
+            if (subject.isPermitted(perm)) {
+                return true;
             }
         }
 
-        return true;
-    }
-
-    /**
-     * 权限过滤实现
-     *
-     * @return
-     */
-    private boolean isAccessAllowedRealization(String[] permArr, Subject subject) {
-        if (permArr.length > 0) {
-            ShiroRealm.ShiroUser user = ((ShiroRealm.ShiroUser) subject.getPrincipals().getPrimaryPrincipal());
-            String key = permArr[0];
-            // 角色判断
-            if (key.equals("role")) {
-                for (int i = 1; i < permArr.length; i++) {
-                    if (user.isEnterprise()) {
-
-                    }
-                }
-            } else if (key.equals("wokeType")) {
-
-            }
-
-
-        }
-        return true;
+        return false;
     }
 
 }
