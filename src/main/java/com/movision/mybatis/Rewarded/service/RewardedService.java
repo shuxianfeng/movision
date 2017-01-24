b7e985fd57b06251b13eeee42e384f1e3754455f
zhurui <244782577@qq.com>
2017-01-24 14:17:18 +0800

    提交用户打赏帖子的接口

---
+++ src/main/java/com/movision/mybatis/Rewarded/service/RewardedService.java
@@ -0,0 +1,32 @@
+package com.movision.mybatis.rewarded.service;
+
+import com.movision.mybatis.rewarded.entity.Rewarded;
+import com.movision.mybatis.rewarded.mapper.RewardedMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author zhurui
+ * @Date 2017/1/24 9:26
+ */
+@Service
+public class RewardedService {
+
+    private Logger log = LoggerFactory.getLogger(RewardedService.class);
+
+    @Autowired
+    RewardedMapper rewardedMapper;
+
+    public int insertRewarded(Rewarded rewarded) {
+        try {
+            log.info("添加打赏记录");
+            return rewardedMapper.insertSelective(rewarded);
+        } catch (Exception e) {
+            log.error("添加打赏记录异常");
+            throw e;
+        }
+    }
+
+}
