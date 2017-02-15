d8e234da784b6495a3cdd0ecc04de053c6b74d8d
shuxf <shuxianfon1987@126.com>
2017-02-15 14:34:46 +0800

    提交商城首页商品分类列表接口

--- src/main/java/com/movision/facade/index/FacadePost.java
+++ src/main/java/com/movision/facade/index/FacadePost.java
@@ -202,11 +202,11 @@ public class FacadePost {
 
                     //这里将获取的路径/WWW/tomcat-8100/apache-tomcat-7.0.73/webapps/movision-1.0.0后缀movision-1.0.0去除
                     //不保存到项目中,防止部包把图片覆盖掉了
-                    String path = savedDir.substring(0, savedDir.length() - 15);
+                    String path = savedDir.substring(0, savedDir.length() - 9);
 
                     //这里组合出真实的图片存储路径
                     String combinpath = path + "/images/post/coverimg";
-                    System.out.println("图片存放路径" + combinpath);
+
 //                    File savedFile = new File(savedDir, savedFileName);
                     File savedFile = new File(combinpath, savedFileName);
                     boolean isCreateSuccess = savedFile.createNewFile();
