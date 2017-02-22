ac07749162be297344a3aef8cb1cf176acdd40ba
ywgitlabadmin <zhuangyuhao711@163.com>
2017-02-22 11:12:51 +0800

    注释不要的接口

--- src/main/java/com/movision/controller/boss/RoleMenuRelationController.java
+++ src/main/java/com/movision/controller/boss/RoleMenuRelationController.java
@@ -1,49 +1,49 @@
-package com.movision.controller.boss;
-
-import com.movision.common.Response;
-import com.movision.facade.user.RoleMenuRelationFacade;
-import com.movision.mybatis.roleMenuRelation.entity.RoleMenuRelation;
-import com.wordnik.swagger.annotations.ApiOperation;
-import com.wordnik.swagger.annotations.ApiParam;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-/**
- * @Author zhuangyuhao
- * @Date 2017/1/24 17:19
- */
-@RestController
-@RequestMapping("boss/role_menu_relation")
-public class RoleMenuRelationController {
-    @Autowired
-    private RoleMenuRelationFacade roleMenuRelationFacade;
-
-    @ApiOperation(value = "新增权限菜单关系", notes = "新增权限菜单关系", response = Response.class)
-    @RequestMapping(value = "add_role_menu_relation", method = RequestMethod.POST)
-    public Response addRoleMenuRelation(@ApiParam @ModelAttribute RoleMenuRelation roleMenuRelation) {
-        Response response = new Response();
-        boolean isAdd = roleMenuRelationFacade.addRoleMenuRelation(roleMenuRelation);
-        if (isAdd) {
-            response.setCode(200);
-        } else {
-            response.setCode(400);
-        }
-        return response;
-    }
-
-    @ApiOperation(value = "批量删除权限菜单关系", notes = "批量删除权限菜单关系", response = Response.class)
-    @RequestMapping(value = "batch_delete", method = RequestMethod.POST)
-    public Response batchDelete(@ApiParam(value = "权限菜单关系表的id,以逗号分隔") @RequestParam String ids) {
-        Response response = new Response();
-        String[] idList = ids.split(",");
-
-        List<String> failDeleteIdList = roleMenuRelationFacade.batchDelete(idList);
-        response.setCode(200);
-        response.setData(failDeleteIdList);
-        return response;
-    }
-
-
-}
+//package com.movision.controller.boss;
+//
+//import com.movision.common.Response;
+//import com.movision.facade.user.RoleMenuRelationFacade;
+//import com.movision.mybatis.roleMenuRelation.entity.RoleMenuRelation;
+//import com.wordnik.swagger.annotations.ApiOperation;
+//import com.wordnik.swagger.annotations.ApiParam;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+///**
+// * @Author zhuangyuhao
+// * @Date 2017/1/24 17:19
+// */
+//@RestController
+//@RequestMapping("boss/role_menu_relation")
+//public class RoleMenuRelationController {
+//    @Autowired
+//    private RoleMenuRelationFacade roleMenuRelationFacade;
+//
+//    /*@ApiOperation(value = "新增权限菜单关系", notes = "新增权限菜单关系", response = Response.class)
+//    @RequestMapping(value = "add_role_menu_relation", method = RequestMethod.POST)
+//    public Response addRoleMenuRelation(@ApiParam @ModelAttribute RoleMenuRelation roleMenuRelation) {
+//        Response response = new Response();
+//        boolean isAdd = roleMenuRelationFacade.addRoleMenuRelation(roleMenuRelation);
+//        if (isAdd) {
+//            response.setCode(200);
+//        } else {
+//            response.setCode(400);
+//        }
+//        return response;
+//    }*/
+//
+//    /*@ApiOperation(value = "批量删除权限菜单关系", notes = "批量删除权限菜单关系", response = Response.class)
+//    @RequestMapping(value = "batch_delete", method = RequestMethod.POST)
+//    public Response batchDelete(@ApiParam(value = "权限菜单关系表的id,以逗号分隔") @RequestParam String ids) {
+//        Response response = new Response();
+//        String[] idList = ids.split(",");
+//
+//        List<String> failDeleteIdList = roleMenuRelationFacade.batchDelete(idList);
+//        response.setCode(200);
+//        response.setData(failDeleteIdList);
+//        return response;
+//    }*/
+//
+//
+//}
--- src/main/java/com/movision/controller/boss/UserRoleRelationController.java
+++ src/main/java/com/movision/controller/boss/UserRoleRelationController.java
@@ -1,39 +1,39 @@
-package com.movision.controller.boss;
-
-import com.movision.common.Response;
-import com.movision.facade.user.UserRoleRelationFacade;
-import com.movision.mybatis.bossMenu.entity.Menu;
-import com.movision.mybatis.userRoleRelation.entity.UserRoleRelation;
-import com.wordnik.swagger.annotations.ApiOperation;
-import com.wordnik.swagger.annotations.ApiParam;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-/**
- * @Author zhuangyuhao
- * @Date 2017/1/24 19:40
- */
-@RestController
-@RequestMapping("boss/user_role_relation")
-public class UserRoleRelationController {
-    @Autowired
-    private UserRoleRelationFacade userRoleRelationFacade;
-
-    @ApiOperation(value = "新增用户角色关系", notes = "新增用户角色关系", response = Response.class)
-    @RequestMapping(value = "add_user_role_relation", method = RequestMethod.POST)
-    public Response addMenu(@ApiParam @ModelAttribute UserRoleRelation userRoleRelation) {
-        Response response = new Response();
-        boolean isAdd = userRoleRelationFacade.addRelation(userRoleRelation);
-        if (isAdd) {
-            response.setCode(200);
-        } else {
-            response.setCode(400);
-        }
-        return response;
-    }
-
-
-}
+//package com.movision.controller.boss;
+//
+//import com.movision.common.Response;
+//import com.movision.facade.user.UserRoleRelationFacade;
+//import com.movision.mybatis.bossMenu.entity.Menu;
+//import com.movision.mybatis.userRoleRelation.entity.UserRoleRelation;
+//import com.wordnik.swagger.annotations.ApiOperation;
+//import com.wordnik.swagger.annotations.ApiParam;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RestController;
+//
+///**
+// * @Author zhuangyuhao
+// * @Date 2017/1/24 19:40
+// */
+//@RestController
+//@RequestMapping("boss/user_role_relation")
+//public class UserRoleRelationController {
+//    @Autowired
+//    private UserRoleRelationFacade userRoleRelationFacade;
+//
+//    @ApiOperation(value = "新增用户角色关系", notes = "新增用户角色关系", response = Response.class)
+//    @RequestMapping(value = "add_user_role_relation", method = RequestMethod.POST)
+//    public Response addMenu(@ApiParam @ModelAttribute UserRoleRelation userRoleRelation) {
+//        Response response = new Response();
+//        boolean isAdd = userRoleRelationFacade.addRelation(userRoleRelation);
+//        if (isAdd) {
+//            response.setCode(200);
+//        } else {
+//            response.setCode(400);
+//        }
+//        return response;
+//    }
+//
+//
+//}
