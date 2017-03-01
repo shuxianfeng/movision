e55fa50052b219cb7b6bbd6c63c0a70f71b951db
zhangl <zhangl@163.com>
2017-03-01 19:20:26 +0800

    提交更新--品牌列表按时间排序

---
+++ src/main/java/com/movision/mybatis/brand/entity/Brand.java
@@ -0,0 +1,65 @@
+package com.movision.mybatis.brand.entity;
+
+import java.util.Date;
+
+public class Brand {
+    private Integer id;
+
+    private Integer protype;
+
+    private Integer brandid;
+
+    private String brandname;
+
+    private Integer isdel;
+
+    private Date intime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getProtype() {
+        return protype;
+    }
+
+    public void setProtype(Integer protype) {
+        this.protype = protype;
+    }
+
+    public Integer getBrandid() {
+        return brandid;
+    }
+
+    public void setBrandid(Integer brandid) {
+        this.brandid = brandid;
+    }
+
+    public String getBrandname() {
+        return brandname;
+    }
+
+    public void setBrandname(String brandname) {
+        this.brandname = brandname == null ? null : brandname.trim();
+    }
+
+    public Integer getIsdel() {
+        return isdel;
+    }
+
+    public void setIsdel(Integer isdel) {
+        this.isdel = isdel;
+    }
+
+    public Date getIntime() {
+        return intime;
+    }
+
+    public void setIntime(Date intime) {
+        this.intime = intime;
+    }
+}
---
+++ src/main/java/com/movision/mybatis/brand/mapper/BrandMapper.java
@@ -0,0 +1,17 @@
+package com.movision.mybatis.brand.mapper;
+
+import com.movision.mybatis.brand.entity.Brand;
+
+public interface BrandMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Brand record);
+
+    int insertSelective(Brand record);
+
+    Brand selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Brand record);
+
+    int updateByPrimaryKey(Brand record);
+}
