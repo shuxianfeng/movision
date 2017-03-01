448bb5cf2ea0557d7c8c9ce29325987a1594def9
zhangl <zhangl@163.com>
2017-03-01 19:39:52 +0800

    提交更新--修改品牌，根据id查询，增加品牌

--- src/main/java/com/movision/mybatis/productcategory/mapper/ProductCategoryMapper.java
+++ src/main/java/com/movision/mybatis/productcategory/mapper/ProductCategoryMapper.java
@@ -28,10 +28,15 @@ public interface ProductCategoryMapper {
 
     int addCategory(ProductCategory productCategory);//添加商品分类
 
+    int addBrand(Brand brand);//添加品牌
+
     ProductCategory queryCategory(Integer id);//根据id查询类别信息
+
+    Brand queryBrand(Integer id);//根据id查询类别信息
 
     int updateCategory(ProductCategory productCategory);//编辑分类
 
+    int updateBrand(Brand brand);//编辑品牌
     List<Brand> findAllBrand(RowBounds rowBounds);//查询品牌列表
 
     List<Brand> findAllBrandCondition(Map map, RowBounds rowBounds);//品牌搜索
