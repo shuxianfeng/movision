abbabe9ccbd3782d54815c8eb44d0a4c9c46b5f0
zhangl <zhangl@163.com>
2017-03-07 14:12:37 +0800

    提交更新--售后操作信息

--- src/main/java/com/movision/mybatis/afterservice/entity/AfterServiceVo.java
+++ src/main/java/com/movision/mybatis/afterservice/entity/AfterServiceVo.java
@@ -33,6 +33,15 @@ public class AfterServiceVo {
     private Integer userid;
 
     private Date proposertime;
+    private Integer status;
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
 
     private Double amountdue;
 
