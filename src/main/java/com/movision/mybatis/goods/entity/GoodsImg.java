7435cb1323551c055040748703d4cf6d05dd76d7
shuxf <shuxianfon1987@126.com>
2017-03-09 17:13:04 +0800

    修改 商品图片实体属性名称修改后的set方法名错误的报错

--- src/main/java/com/movision/mybatis/goods/entity/GoodsImg.java
+++ src/main/java/com/movision/mybatis/goods/entity/GoodsImg.java
@@ -8,7 +8,7 @@ public class GoodsImg {
 
     private Integer goodsid;//商品id
 
-    private String img_url;//图片地址url
+    private String imgurl;//图片地址url
 
     private Integer type;//图片类型
 
@@ -20,14 +20,6 @@ public class GoodsImg {
 
     public void setGoodsid(Integer goodsid) {
         this.goodsid = goodsid;
-    }
-
-    public String getImg_url() {
-        return img_url;
-    }
-
-    public void setImg_url(String img_url) {
-        this.img_url = img_url;
     }
 
     public Integer getType() {
@@ -44,5 +36,13 @@ public class GoodsImg {
 
     public void setOderid(Integer oderid) {
         this.oderid = oderid;
+    }
+
+    public String getImgurl() {
+        return imgurl;
+    }
+
+    public void setImgurl(String imgurl) {
+        this.imgurl = imgurl;
     }
 }
