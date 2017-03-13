33cb576b0f6ca2a21beaa77c71a69b0759c26276
ywgitlabadmin <zhuangyuhao711@163.com>
2017-03-13 13:58:43 +0800

    上传视频

--- src/main/java/com/movision/facade/video/VideoFacade.java
+++ src/main/java/com/movision/facade/video/VideoFacade.java
@@ -87,11 +87,11 @@ public class VideoFacade {
             String data = null;
             //可以上传多个文件
             log.info("请求消息中的所有参数名的Enumeration对象" + request.getParameterNames() + ", 求消息中的所有参数名和值的Map对象" +
-                    request.getParameterMap().toString());
-            Map<String, String> requestMap = request.getParameterMap();
+                    String.valueOf(request.getParameterMap()));
+            Map<String, Object> requestMap = request.getParameterMap();
             // TODO: 2017/3/13 下面是获取请求中的参数
-            for (Map.Entry<String, String> entry : requestMap.entrySet()) {
-                log.info("键值对是：" + entry.getKey() + ", " + entry.getValue());
+            for (Map.Entry<String, Object> entry : requestMap.entrySet()) {
+                log.info("键值对是：" + String.valueOf(entry.getKey()) + ", " + String.valueOf(entry.getValue()));
             }
             List<FileItem> list = upload.parseRequest(request);
             log.info("List<FileItem>=" + list);
