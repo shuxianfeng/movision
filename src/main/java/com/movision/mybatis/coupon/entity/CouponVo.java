3f13028a25b777950619e579b0b4ef729832c694
zhurui <244782577@qq.com>
2017-03-31 15:14:54 +0800

    后台管理-更新查询用户优惠券列表

--- src/main/java/com/movision/mybatis/coupon/entity/CouponVo.java
+++ src/main/java/com/movision/mybatis/coupon/entity/CouponVo.java
@@ -13,9 +13,13 @@ public class CouponVo {
 
     private String storename;//店铺名称
 
-    private Integer category;//范围
-
     private Integer statue;//状态
+
+    private Date begintime;//开始时间
+
+    private Date endtime;//结束时间
+
+    private Date intime;//优惠券放券时间
 
     private Double tmoney;//金额
 
@@ -23,8 +27,99 @@ public class CouponVo {
 
     private Integer isdel;//是否删除
 
-    private Integer amount;//张数
+    public Integer getId() {
+        return id;
+    }
 
-    private Integer deadline;//期限
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getStorename() {
+        return storename;
+    }
+
+    public void setStorename(String storename) {
+        this.storename = storename;
+    }
+
+    public Integer getStatue() {
+        return statue;
+    }
+
+    public void setStatue(Integer statue) {
+        this.statue = statue;
+    }
+
+    public Date getBegintime() {
+        return begintime;
+    }
+
+    public void setBegintime(Date begintime) {
+        this.begintime = begintime;
+    }
+
+    public Date getEndtime() {
+        return endtime;
+    }
+
+    public void setEndtime(Date endtime) {
+        this.endtime = endtime;
+    }
+
+    public Date getIntime() {
+        return intime;
+    }
+
+    public void setIntime(Date intime) {
+        this.intime = intime;
+    }
+
+    public Double getTmoney() {
+        return tmoney;
+    }
+
+    public void setTmoney(Double tmoney) {
+        this.tmoney = tmoney;
+    }
+
+    public Double getUsemoney() {
+        return usemoney;
+    }
+
+    public void setUsemoney(Double usemoney) {
+        this.usemoney = usemoney;
+    }
+
+    public Integer getIsdel() {
+        return isdel;
+    }
+
+    public void setIsdel(Integer isdel) {
+        this.isdel = isdel;
+    }
 }
