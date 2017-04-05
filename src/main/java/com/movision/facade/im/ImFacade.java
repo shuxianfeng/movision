747bf030b509d261df9315d5e8ca4b3e3ecbec3b
zhangl <zhangl@163.com>
2017-04-05 17:45:07 +0800

    更新状态---系统推送

--- src/main/java/com/movision/facade/im/ImFacade.java
+++ src/main/java/com/movision/facade/im/ImFacade.java
@@ -544,7 +544,7 @@ public class ImFacade {
         imSystemInform.setTitle(title);
         imSystemInform.setPushcontent(pushcontent);
         imSystemInform.setInformTime(new Date());
-        imSystemInform.setIsread(0);
+        imSystemInform.setIsread(1);
         //每次取500个人
         imSystemInformService.add(imSystemInform);
     }
