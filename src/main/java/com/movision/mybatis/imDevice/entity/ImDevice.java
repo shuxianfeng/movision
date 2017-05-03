a629ce73f5103759c87357cc60155ea7d901c4be
ywgitlabadmin <zhuangyuhao711@163.com>
2017-05-03 10:23:21 +0800

    1

---
+++ src/main/java/com/movision/mybatis/imDevice/entity/ImDevice.java
@@ -0,0 +1,85 @@
+package com.movision.mybatis.imDevice.entity;
+
+import java.util.Date;
+
+public class ImDevice {
+    private Integer id;
+
+    private Integer deviceid;
+
+    private String accid;
+
+    private String token;
+
+    private String name;
+
+    private String icon;
+
+    private String sign;
+
+    private Date createTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getDeviceid() {
+        return deviceid;
+    }
+
+    public void setDeviceid(Integer deviceid) {
+        this.deviceid = deviceid;
+    }
+
+    public String getAccid() {
+        return accid;
+    }
+
+    public void setAccid(String accid) {
+        this.accid = accid == null ? null : accid.trim();
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token == null ? null : token.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon == null ? null : icon.trim();
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign == null ? null : sign.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
