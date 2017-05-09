2f88a5ed752ad6cd4e08d5095fc06af2c10b4ab1
shuxf <shuxianfon1987@126.com>
2017-05-09 11:13:21 +0800

    优化视频转码工具类

--- src/main/java/com/movision/controller/app/AppJUnitTest.java
+++ src/main/java/com/movision/controller/app/AppJUnitTest.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Map;
 
