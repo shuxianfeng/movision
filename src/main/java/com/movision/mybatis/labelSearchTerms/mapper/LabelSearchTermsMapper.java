47fad3e07c21b8e82e5cd6e64a1326e0e274f00b
zhangl <zhangl@163.com>
2017-07-28 16:02:10 +0800

    更新状态---标签搜索历史

---
+++ src/main/java/com/movision/mybatis/labelSearchTerms/mapper/LabelSearchTermsMapper.java
@@ -0,0 +1,13 @@
+package com.movision.mybatis.labelSearchTerms.mapper;
+
+import com.movision.mybatis.labelSearchTerms.entity.LabelSearchTerms;
+
+
+/**
+ * @Author zhanglei
+ * @Date 2017/7/28 15:49
+ */
+public interface LabelSearchTermsMapper {
+
+    public void insert(LabelSearchTerms labelSearchTerms);
+}
