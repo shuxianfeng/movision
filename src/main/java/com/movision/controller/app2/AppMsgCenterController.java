bd22060ef1d227b007f3a5118c7258bf50af66c1
ywgitlabadmin <zhuangyuhao711@163.com>
2017-08-02 11:25:17 +0800

    啦啦啦啦啦啦

---
+++ src/main/java/com/movision/controller/app2/AppMsgCenterController.java
@@ -0,0 +1,15 @@
+package com.movision.controller.app2;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author zhuangyuhao
+ * @Date 2017/8/2 11:22
+ */
+@RestController
+@RequestMapping("app/msg_center")
+public class AppMsgCenterController {
+
+
+}
--- src/main/java/com/movision/utils/DateUtils.java
+++ src/main/java/com/movision/utils/DateUtils.java
@@ -272,23 +272,5 @@ public class DateUtils {
 //        getCurrentMonthFirstDay();
         getCurrentMonthLastDay();
 
-//        Date date = DateUtils.date2Sub(DateUtils.str2Date("2016-03-02 20:16:21", "yyyy-MM-dd HH:mm:ss"), 12, 10);
-//        System.out.println(date);
-        // Date date1 = new Date();
-        // System.out.println(date1);
-        // System.out.println(date1.before(date));
-        // String start = "2016-06-20 16:42:41";
-        // String end = "2017-06-21 00:00:00";
-        // SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd");
-        // Date startTime = sf.parse(start);
-        // Date endTime = sf.parse(end);
-        // long days = dayDiff(startTime, endTime);
-        // System.out.println(days);
-//         String s = DateUtils.str2DateFormat("1468466182000", "yyyy-MM-dd");
-//         System.out.println(s);
-
-//        System.out.println(DateUtils.str2Date("20170129023128", "yyyyMMddHHmmss"));
-//        System.out.println(str2Date("1991-07-11"));
-//        System.out.println(getCurrentDate());
     }
 }
