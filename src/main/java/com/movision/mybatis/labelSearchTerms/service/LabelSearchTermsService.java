c65bc7b0cf9cc797d9bb0f1c343d2c3d5096aa4e
zhangl <zhangl@163.com>
2017-08-10 13:56:18 +0800

    更新状态---标签

--- src/main/java/com/movision/mybatis/labelSearchTerms/service/LabelSearchTermsService.java
+++ src/main/java/com/movision/mybatis/labelSearchTerms/service/LabelSearchTermsService.java
@@ -56,6 +56,13 @@ public class LabelSearchTermsService implements LabelSearchTermsMapper {
 
             cursor = table.find(queryObject, keys).limit(12).sort(new BasicDBObject("intime", -1));
             list = cursor.toArray();
+            for (int i = 0; i < list.size(); i++) {
+                for (int j = list.size() - 1; j > i; j--) {
+                    if (list.get(i).get("labelid").equals(list.get(j).get("labelid"))) {
+                        list.remove(j);
+                    }
+                }
+            }
             cursor.close();
         } catch (Exception e) {
             log.error("查询标签搜索历史记录", e);
