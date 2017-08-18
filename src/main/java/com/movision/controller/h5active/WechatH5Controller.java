078bd9f4a8a7e91e6702d0b8dd99cdd1cfef9de7
shuxf <shuxianfon1987@126.com>
2017-08-18 17:27:01 +0800

    提交图片合成接口

---
+++ src/main/java/com/movision/controller/h5active/WechatH5Controller.java
@@ -0,0 +1,45 @@
+package com.movision.controller.h5active;
+
+import com.movision.common.Response;
+import com.movision.facade.h5wechat.WechatH5Facade;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * @Author shuxf
+ * @Date 2017/8/18 15:44
+ * 微信公众号H5活动接口控制器
+ */
+@RestController
+@RequestMapping("/h5/wechat/")
+public class WechatH5Controller {
+
+    @Autowired
+    private WechatH5Facade wechatH5Facade;
+
+    @ApiOperation(value = "图片合成模板H5接口(结婚证模板)", notes = "图片合成模板H5接口", response = Response.class)
+    @RequestMapping(value = "imgCompose", method = RequestMethod.POST)
+    public Response imgCompose(@ApiParam(value = "男的名字") @RequestParam String manname,
+                                        @ApiParam(value = "女的名字") @RequestParam String womanname){
+        Response response = new Response();
+
+        Map<String, Object> map = wechatH5Facade.imgCompose(manname, womanname);
+        if ((Integer)map.get("status")==200){
+            response.setMessage("合成成功");
+            response.setCode((Integer)map.get("status"));
+            response.setData(map);
+        }else{
+            response.setMessage("合成失败");
+            response.setCode((Integer)map.get("status"));
+        }
+
+        return response;
+    }
+}
