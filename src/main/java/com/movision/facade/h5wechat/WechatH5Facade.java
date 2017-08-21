41ad9f476835162eaa059f281c1531aa01de0f77
zhangl <zhangl@163.com>
2017-08-21 17:00:53 +0800

    更新状态---标签详情

--- src/main/java/com/movision/facade/h5wechat/WechatH5Facade.java
+++ src/main/java/com/movision/facade/h5wechat/WechatH5Facade.java
@@ -98,8 +98,8 @@ public class WechatH5Facade {
                 g.setFont(f);
 
                 //10,20 表示这段文字在图片上的位置(x,y) .第一个是你设置的内容。
-                g.drawString(manname, 160, 610);//合成男的名字
-                g.drawString(womanname, 160, 720);//合成女的名字
+                g.drawString(new String(manname.getBytes("utf8"), "gbk"), 160, 610);//合成男的名字new String(message.getBytes("utf8"),"gbk");
+                g.drawString(new String(womanname.getBytes("utf8"), "gbk"), 160, 720);//合成女的名字
 
                 g.dispose();
 
