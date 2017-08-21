37ee650318f9c5b67f678b7aca361debb6e2eb54
zhangl <zhangl@163.com>
2017-08-21 18:00:01 +0800

    更新状态---标签详情

--- src/main/java/com/movision/facade/h5wechat/WechatH5Facade.java
+++ src/main/java/com/movision/facade/h5wechat/WechatH5Facade.java
@@ -28,6 +28,11 @@ import java.util.Map;
 @Service
 public class WechatH5Facade {
     private static Logger log = LoggerFactory.getLogger(WechatH5Facade.class);
+    //下面是模板图片的路径
+    String timgurl = PropertiesLoader.getValue("wechat.h5.domain");
+    String newurl = PropertiesLoader.getValue("wechat.newh5.domain");//新图片路径
+    String headImg = PropertiesLoader.getValue("wechat.erweima.domain");//二维码路径
+    String lihunurl = PropertiesLoader.getValue("wechat.lihun.domain");
 
     @Autowired
     private MovisionOssClient movisionOssClient;
@@ -62,62 +67,12 @@ public class WechatH5Facade {
 //                e.printStackTrace();
 //            }
 //        }
-        //下面是模板图片的路径
-        String timgurl = PropertiesLoader.getValue("wechat.h5.domain");
-        String newurl = PropertiesLoader.getValue("wechat.newh5.domain");//新图片路径
-        String headImg = PropertiesLoader.getValue("wechat.erweima.domain");//二维码路径
 
 //        (String username,String headImg)
         if (type == 1) {//结婚证
             try {
                 InputStream is = new FileInputStream(timgurl);
-                //通过JPEG图象流创建JPEG数据流解码器
-                JPEGImageDecoder jpegDecoder = JPEGCodec.createJPEGDecoder(is);
-                //解码当前JPEG数据流，返回BufferedImage对象
-                BufferedImage buffImg = jpegDecoder.decodeAsBufferedImage();
-                //得到画笔对象
-                //Graphics g = buffImg.getGraphics();
-                Graphics2D g = (Graphics2D) buffImg.getGraphics();
-                //创建你要附加的图象。//-----------------------------------------------这一段是将小图片合成到大图片上的代码
-                //小图片的路径
-                ImageIcon imgIcon = new ImageIcon(headImg);
-                //得到Image对象。
-                Image img = imgIcon.getImage();
-                //将小图片绘到大图片上。
-                //5,300 .表示你的小图片在大图片上的位置。
-                g.drawImage(img, 400, 15, null);
-
-                //设置颜色。
-                g.setColor(Color.BLACK);
-
-                //最后一个参数用来设置字体的大小
-                Font f = new Font("宋体", Font.PLAIN, 25);
-                Color mycolor = Color.BLACK;//new Color(0, 0, 255);
-                g.setColor(mycolor);
-                g.setFont(f);
-
-                //10,20 表示这段文字在图片上的位置(x,y) .第一个是你设置的内容。
-                g.drawString(manname, 160, 610);//合成男的名字new String(message.getBytes("utf8"),"gbk");
-                g.drawString(womanname, 160, 720);//合成女的名字
-
-                g.dispose();
-
-                //OutputStream os;
-
-                //os = new FileOutputStream("d:/union.jpg");
-                String shareFileName = System.currentTimeMillis() + ".jpg";
-
-                map.put("status", 200);
-                map.put("url", shareFileName);
-                String url = newurl + shareFileName;
-                //  os = new FileOutputStream(shareFileName);
-                //创键编码器，用于编码内存中的图象数据。
-                //JPEGImageEncoder en = JPEGCodec.createJPEGEncoder(os);
-                // en.encode(buffImg);
-                ImageIO.write(buffImg, "png", new File(url));//图片的输出路径
-                map.put("newurl", url);
-                is.close();
-                //  os.close();
+                map = He(is, manname, womanname);
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             } catch (ImageFormatException e) {
@@ -126,8 +81,80 @@ public class WechatH5Facade {
                 e.printStackTrace();
             }
         } else if (type == 2) {//离婚证
-
+            try {
+                InputStream is = new FileInputStream(lihunurl);
+                map = He(is, manname, womanname);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (ImageFormatException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         return map;
     }
+
+
+    public Map He(InputStream is, String manname, String womanname) {
+        Map map = new HashMap();
+        try {
+
+            //通过JPEG图象流创建JPEG数据流解码器
+            JPEGImageDecoder jpegDecoder = JPEGCodec.createJPEGDecoder(is);
+            //解码当前JPEG数据流，返回BufferedImage对象
+            BufferedImage buffImg = jpegDecoder.decodeAsBufferedImage();
+            //得到画笔对象
+            //Graphics g = buffImg.getGraphics();
+            Graphics2D g = (Graphics2D) buffImg.getGraphics();
+            //创建你要附加的图象。//-----------------------------------------------这一段是将小图片合成到大图片上的代码
+            //小图片的路径
+            ImageIcon imgIcon = new ImageIcon(headImg);
+            //得到Image对象。
+            Image img = imgIcon.getImage();
+            //将小图片绘到大图片上。
+            //5,300 .表示你的小图片在大图片上的位置。
+            g.drawImage(img, 400, 15, null);
+
+            //设置颜色。
+            g.setColor(Color.BLACK);
+
+            //最后一个参数用来设置字体的大小
+            Font f = new Font("宋体", Font.PLAIN, 25);
+            Color mycolor = Color.BLACK;//new Color(0, 0, 255);
+            g.setColor(mycolor);
+            g.setFont(f);
+
+            //10,20 表示这段文字在图片上的位置(x,y) .第一个是你设置的内容。
+            g.drawString(manname, 160, 610);//合成男的名字new String(message.getBytes("utf8"),"gbk");
+            g.drawString(womanname, 160, 720);//合成女的名字
+
+            g.dispose();
+
+            //OutputStream os;
+
+            //os = new FileOutputStream("d:/union.jpg");
+            String shareFileName = System.currentTimeMillis() + ".jpg";
+
+            map.put("status", 200);
+            map.put("url", shareFileName);
+            String url = newurl + shareFileName;
+            //  os = new FileOutputStream(shareFileName);
+            //创键编码器，用于编码内存中的图象数据。
+            //JPEGImageEncoder en = JPEGCodec.createJPEGEncoder(os);
+            // en.encode(buffImg);
+            ImageIO.write(buffImg, "png", new File(url));//图片的输出路径
+            map.put("newurl", url);
+            is.close();
+            //  os.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (ImageFormatException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
+
 }
