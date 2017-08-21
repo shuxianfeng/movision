c6d5b47e839c17e9716c479043c97ff6356063ca
shuxf <shuxianfon1987@126.com>
2017-08-21 09:46:50 +0800

    修改图片合成接口

--- src/main/java/com/movision/facade/h5wechat/WechatH5Facade.java
+++ src/main/java/com/movision/facade/h5wechat/WechatH5Facade.java
@@ -23,37 +23,6 @@ public class WechatH5Facade {
 
     public Map<String, Object> imgCompose(String manname, String womanname) {
         Map<String, Object> map = new HashMap<>();
-//        public static void exportImg1(){
-//            int width = 100;
-//            int height = 100;
-//            String s = "你好";
-//
-//            File file = new File("d:/image.jpg");
-//
-//            Font font = new Font("Serif", Font.BOLD, 10);
-//            BufferedImage bi = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-//            Graphics2D g2 = (Graphics2D)bi.getGraphics();
-//            g2.setBackground(Color.WHITE);
-//            g2.clearRect(0, 0, width, height);
-//            g2.setPaint(Color.RED);
-//
-//            FontRenderContext context = g2.getFontRenderContext();
-//            Rectangle2D bounds = font.getStringBounds(s, context);
-//            double x = (width - bounds.getWidth()) / 2;
-//            double y = (height - bounds.getHeight()) / 2;
-//            double ascent = -bounds.getY();
-//            double baseY = y + ascent;
-//
-//            g2.drawString(s, (int)x, (int)baseY);
-//
-//            try {
-//                ImageIO.write(bi, "jpg", file);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-
-//        (String username,String headImg)
         try {
             //下面是模板图片的路径
             String timgurl = PropertiesLoader.getValue("wechat.h5.domain");
