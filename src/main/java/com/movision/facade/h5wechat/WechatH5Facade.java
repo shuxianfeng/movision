ae1ec630c85f5822fa1e89fb88293b4d8595d572
zhangl <zhangl@163.com>
2017-08-22 18:39:45 +0800

    更新状态---标签详情

--- src/main/java/com/movision/facade/h5wechat/WechatH5Facade.java
+++ src/main/java/com/movision/facade/h5wechat/WechatH5Facade.java
@@ -140,8 +140,8 @@ public class WechatH5Facade extends JPanel {
             for (int i = 0; i < 1; i++) {
                 g.rotate(70 * Math.PI / 180);
                 g.setPaint(mycolor[i % 2]);
-                g.drawString(manname, -550, -900);
-                g.drawString(womanname, -550, -700);
+                g.drawString(manname, -550, 900);
+                g.drawString(womanname, -550, 700);
             }
             g.dispose();
 
