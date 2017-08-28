e6d3addfe9fddd3447569bea9c5fbeb624759bd9
shuxf <shuxianfon1987@126.com>
2017-08-28 10:47:08 +0800

    修改我的模块中的关注和粉丝入参注释

--- src/main/java/com/movision/controller/app2/AppMineController.java
+++ src/main/java/com/movision/controller/app2/AppMineController.java
@@ -43,7 +43,7 @@ public class AppMineController {
 
     @ApiOperation(value = "我的--关注--关注的圈子，点击关注调用的关注的圈子接口", notes = "关注的圈子部分、关注的作者部分、关注的标签部分", response = Response.class)
     @RequestMapping(value = "myfollowcircle", method = RequestMethod.POST)
-    public Response getMineFollowCircle(@ApiParam(value = "用户id(必填，否则无法进入‘我的’页面)") @RequestParam String userid,
+    public Response getMineFollowCircle(@ApiParam(value = "用户id(必填，否则无法进入‘我的’页面或无法查看哪个用户的主页)") @RequestParam String userid,
                                         @ApiParam(value = "第几页") @RequestParam(required = false, defaultValue = "1") String pageNo,
                                         @ApiParam(value = "每页多少条") @RequestParam(required = false, defaultValue = "10") String pageSize){
         Response response = new Response();
@@ -60,7 +60,7 @@ public class AppMineController {
 
     @ApiOperation(value = "我的--关注--关注的作者，点击关注调用的关注的作者接口", notes = "关注的作者部分", response = Response.class)
     @RequestMapping(value = "myfollowauthor", method = RequestMethod.POST)
-    public Response getMineFollowAuthor(@ApiParam(value = "用户id(必填，否则无法进入‘我的’页面)") @RequestParam String userid,
+    public Response getMineFollowAuthor(@ApiParam(value = "用户id(必填，否则无法进入‘我的’页面或无法查看哪个用户的主页)") @RequestParam String userid,
                                         @ApiParam(value = "第几页") @RequestParam(required = false, defaultValue = "1") String pageNo,
                                         @ApiParam(value = "每页多少条") @RequestParam(required = false, defaultValue = "10") String pageSize){
         Response response = new Response();
@@ -77,7 +77,7 @@ public class AppMineController {
 
     @ApiOperation(value = "我的--关注--关注的标签，点击关注调用的关注的标签接口", notes = "关注的标签部分", response = Response.class)
     @RequestMapping(value = "myfollowlabel", method = RequestMethod.POST)
-    public Response getMineFollowLabel(@ApiParam(value = "用户id(必填，否则无法进入‘我的’页面)") @RequestParam String userid,
+    public Response getMineFollowLabel(@ApiParam(value = "用户id(必填，否则无法进入‘我的’页面或无法查看哪个用户的主页)") @RequestParam String userid,
                                        @ApiParam(value = "第几页") @RequestParam(required = false, defaultValue = "1") String pageNo,
                                        @ApiParam(value = "每页多少条") @RequestParam(required = false, defaultValue = "10") String pageSize){
         Response response = new Response();
@@ -94,7 +94,7 @@ public class AppMineController {
 
     @ApiOperation(value = "我的模块——点击粉丝，进入用户被关注的粉丝用户列表接口", notes = "用户被关注的粉丝用户列表接口", response = Response.class)
     @RequestMapping(value = "myfans", method = RequestMethod.POST)
-    public Response getMyfans(@ApiParam(value = "用户id(必填，否则无法进入‘我的’页面)") @RequestParam String userid,
+    public Response getMyfans(@ApiParam(value = "用户id(必填，否则无法进入‘我的’页面或无法查看哪个用户的主页)") @RequestParam String userid,
                               @ApiParam(value = "第几页") @RequestParam(required = false, defaultValue = "1") String pageNo,
                               @ApiParam(value = "每页多少条") @RequestParam(required = false, defaultValue = "10") String pageSize){
         Response response = new Response();
