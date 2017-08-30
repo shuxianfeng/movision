aefff35ec82f93a87f040a56234f398a2613ece8
zhurui <244782577@qq.com>
2017-08-30 09:48:22 +0800

    更新投票实体及xml

---
+++ src/main/java/com/movision/controller/boss/VoteSystemController.java
@@ -0,0 +1,8 @@
+package com.movision.controller.boss;
+
+/**
+ * @Author zhurui
+ * @Date 2017/8/30 9:35
+ */
+public class VoteSystemController {
+}
--- src/main/java/com/movision/mybatis/take/entity/Take.java
+++ src/main/java/com/movision/mybatis/take/entity/Take.java
@@ -13,6 +13,65 @@ public class Take {
 
     private Date intime;
 
+    private String phone;//电话
+
+    private String photo;//封面
+
+    private String describe;//描述
+
+    private String nickname;//用户名
+
+    private Integer mark;//排序号
+
+    private Integer audit;//审核
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getDescribe() {
+        return describe;
+    }
+
+    public void setDescribe(String describe) {
+        this.describe = describe;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public Integer getMark() {
+        return mark;
+    }
+
+    public void setMark(Integer mark) {
+        this.mark = mark;
+    }
+
+    public Integer getAudit() {
+        return audit;
+    }
+
+    public void setAudit(Integer audit) {
+        this.audit = audit;
+    }
 
     public Integer getId() {
         return id;
