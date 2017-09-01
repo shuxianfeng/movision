c5b897768d64a2cb83a500b3508fc9638ed8372b
zhangl <zhangl@163.com>
2017-09-01 12:56:49 +0800

    更新状态---标签详情

--- src/main/java/com/movision/facade/h5wechat/WechatH5Facade.java
+++ src/main/java/com/movision/facade/h5wechat/WechatH5Facade.java
@@ -267,7 +267,7 @@ public class WechatH5Facade extends JPanel {
 
             g.fillRect(0, 0, getWidth(), getHeight());
             g.rotate(-5, 0, 0);
-            g.drawImage(img, 0, 0, this);
+            g.drawImage(img, 80, 880, this);
             //g.rotate(30);
             //设置颜色。
             g.setColor(Color.BLACK);
