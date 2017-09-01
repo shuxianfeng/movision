3ebaee8610beca783ba566455d35477b31976a25
zhangl <zhangl@163.com>
2017-09-01 10:37:32 +0800

    更新状态---标签详情

--- src/main/java/com/movision/facade/h5wechat/WechatH5Facade.java
+++ src/main/java/com/movision/facade/h5wechat/WechatH5Facade.java
@@ -273,7 +273,7 @@ public class WechatH5Facade extends JPanel {
             g.setColor(Color.BLACK);
 
             //最后一个参数用来设置字体的大小
-            Font f = new Font("方正静蕾简体", Font.PLAIN, 15);
+            Font f = new Font("xjl", Font.PLAIN, 15);
             Color color = new Color(112, 104, 115);
             Color[] mycolor = {color, Color.LIGHT_GRAY};
             // g.setColor(mycolor);
