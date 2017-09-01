60afb42c9853aa553715865ccee0029007a3db89
zhangl <zhangl@163.com>
2017-09-01 11:11:59 +0800

    更新状态---标签详情

--- src/main/java/com/movision/facade/h5wechat/WechatH5Facade.java
+++ src/main/java/com/movision/facade/h5wechat/WechatH5Facade.java
@@ -292,7 +292,7 @@ public class WechatH5Facade extends JPanel {
                 g.drawString(womanname, 210, 208);
                 g.drawString(manname, 266, 809);
                 g.drawString(womanname, 620, 812);
-                g.drawString(content, 310, 265);
+                g.drawString(content, 290, 270);
                 g.drawString(year, 113, 850);
                 g.drawString(month, 187, 850);
                 g.drawString(day, 220, 850);
