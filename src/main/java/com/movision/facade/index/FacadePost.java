7ec28a75e1d7983d3c9fad91944ddab9c46f0e07
zhangl <zhangl@163.com>
2017-09-13 11:56:34 +0800

    更新状态---标签详情

--- src/main/java/com/movision/facade/index/FacadePost.java
+++ src/main/java/com/movision/facade/index/FacadePost.java
@@ -350,11 +350,11 @@ public class FacadePost {
             active.setPartsum(postpartsum);
         }
         //用户有没有投过稿
-        Map map = new HashMap();
+        /**Map map = new HashMap();
         map.put("id", Integer.parseInt(postid));
         map.put("uid", ShiroUtil.getAppUserID());
         int ispart = postService.isUserContribe(map);
-        active.setIsPart(ispart);
+         active.setIsPart(ispart);*/
         //如果为商城促销类活动，需要在此基础上增加促销类商品列表
         if (activetype.equals("1")) {
 
