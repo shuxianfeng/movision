533c3115c0c52c830adf7f36b01f4dc54e7c326d
zhangl <zhangl@163.com>
2017-09-22 13:48:58 +0800

    更新状态---标签详情

---
+++ src/main/java/com/movision/mybatis/robotComment/entity/RobotComment.java
@@ -0,0 +1,33 @@
+package com.movision.mybatis.robotComment.entity;
+
+public class RobotComment {
+    private Integer id;
+
+    private String content;
+
+    private Integer type;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content == null ? null : content.trim();
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+}
