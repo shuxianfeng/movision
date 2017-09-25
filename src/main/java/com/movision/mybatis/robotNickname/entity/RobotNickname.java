51571fd36907944b41acf0a27b002cb1b5dd8fbf
zhurui <244782577@qq.com>
2017-09-25 15:10:25 +0800

    新增机器人评论帖子

---
+++ src/main/java/com/movision/mybatis/robotNickname/entity/RobotNickname.java
@@ -0,0 +1,23 @@
+package com.movision.mybatis.robotNickname.entity;
+
+public class RobotNickname {
+    private Integer id;
+
+    private String nickname;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname == null ? null : nickname.trim();
+    }
+}
---
+++ src/main/java/com/movision/mybatis/robotNickname/mapper/RobotNicknameMapper.java
@@ -0,0 +1,21 @@
+package com.movision.mybatis.robotNickname.mapper;
+
+import com.movision.mybatis.robotNickname.entity.RobotNickname;
+
+import java.util.List;
+
+public interface RobotNicknameMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(RobotNickname record);
+
+    int insertSelective(RobotNickname record);
+
+    RobotNickname selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(RobotNickname record);
+
+    int updateByPrimaryKey(RobotNickname record);
+
+    List<String> queryRoboltNickname(Integer number);
+}
