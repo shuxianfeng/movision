3f5d48f5b052610257f5a46a5f99f3c9e3c4fade
zhangl <zhangl@163.com>
2017-10-17 16:12:01 +0800

    更新状态---修改平台访问日志

--- src/main/java/com/movision/aop/AspectLog.java
+++ src/main/java/com/movision/aop/AspectLog.java
@@ -1,5 +1,7 @@
 package com.movision.aop;
 
+import com.movision.mybatis.accessLog.entity.AccessLog;
+import com.movision.mybatis.accessLog.service.AccessLogService;
 import com.movision.shiro.realm.ShiroRealm.ShiroUser;
 import com.movision.utils.propertiesLoader.PropertiesLoader;
 import org.apache.shiro.SecurityUtils;
@@ -10,11 +12,14 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * 平台访问日志
@@ -25,6 +30,10 @@ import javax.servlet.http.HttpServletRequest;
 @Component
 @Aspect
 public class AspectLog {
+
+    @Autowired
+    private AccessLogService accessLogService;
+
     private static final Logger log = LoggerFactory.getLogger(AspectLog.class);
 
     ThreadLocal<Long> time = new ThreadLocal<Long>();
@@ -51,29 +60,29 @@ public class AspectLog {
         String requestURL = request.getRequestURL().toString();
         String queryString = request.getQueryString();
         String userAgent = request.getHeader("User-Agent");
-
+        String[] aa = requestURL.split("/");
+        String busitype = aa[3];
         String logMode = PropertiesLoader.getValue("busi.log.mode");
-
         switch (logMode) {
             case "db":
-                /*AccessLog accessLog = new AccessLog();
-                accessLog.setMemberId(memberId);    //用户id
-                accessLog.setClientIP(clientIP);    //客户端ip
-                accessLog.setHttpMethod(httpMethod);    //http请求方法
-                accessLog.setRequestURL(requestURL);    //请求url
-                accessLog.setQueryString(queryString);  //请求参数
-                accessLog.setUserAgent(userAgent);  //用户代理
-                accessLog.setExecTime(execTime);    //执行日期
-
-                int isAdd = accessLogService.addAccessLog(accessLog);*/
+                AccessLog accessLog = new AccessLog();
+                accessLog.setMemberid(Integer.parseInt(String.valueOf(memberId)));    //用户id
+                accessLog.setClientip(clientIP);    //客户端ip
+                accessLog.setHttpmethod(httpMethod);    //http请求方法
+                accessLog.setRequesturl(requestURL);    //请求url
+                accessLog.setQuerystring(queryString);  //请求参数
+                accessLog.setUseragent(userAgent);  //用户代理
+                accessLog.setExectime(Integer.parseInt(String.valueOf(execTime)));    //执行日期
+                accessLog.setBusitype(busitype);//业务类型
+                int isAdd = accessLogService.insertSelective(accessLog);
                 // TODO: 2017/1/16
 
 
-//                log.debug("AspectLog增加访问日志->isAdd=" + isAdd);
+                log.debug("AspectLog增加访问日志->isAdd=" + isAdd);
                 break;
             case "file":
-                log.trace("memberId:[{}]&clientIP:[{}]&httpMethod:[{}]&requestURL:[{}]&queryString[{}]&userAgent[{}]&execTime[{}]",
-                        new Object[]{memberId, clientIP, httpMethod, requestURL, queryString, userAgent, execTime});
+                log.trace("memberId:[{}]&clientIP:[{}]&httpMethod:[{}]&requestURL:[{}]&queryString[{}]&userAgent[{}]&execTime[{}]&busitype[{}]",
+                        new Object[]{memberId, clientIP, httpMethod, requestURL, queryString, userAgent, execTime, busitype});
                 break;
             default:
                 log.error("业务日志配置不正确");
---
+++ src/main/java/com/movision/mybatis/accessLog/entity/AccessLog.java
@@ -0,0 +1,95 @@
+package com.movision.mybatis.accessLog.entity;
+
+import java.util.Date;
+
+public class AccessLog {
+    private Integer id;
+
+    private Integer memberid;
+
+    private String clientip;
+
+    private String httpmethod;
+
+    private String requesturl;
+
+    private String querystring;
+
+    private String useragent;
+
+    private Integer exectime;
+
+    private String busitype;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getMemberid() {
+        return memberid;
+    }
+
+    public void setMemberid(Integer memberid) {
+        this.memberid = memberid;
+    }
+
+    public String getClientip() {
+        return clientip;
+    }
+
+    public void setClientip(String clientip) {
+        this.clientip = clientip == null ? null : clientip.trim();
+    }
+
+    public String getHttpmethod() {
+        return httpmethod;
+    }
+
+    public void setHttpmethod(String httpmethod) {
+        this.httpmethod = httpmethod == null ? null : httpmethod.trim();
+    }
+
+    public String getRequesturl() {
+        return requesturl;
+    }
+
+    public void setRequesturl(String requesturl) {
+        this.requesturl = requesturl == null ? null : requesturl.trim();
+    }
+
+    public String getQuerystring() {
+        return querystring;
+    }
+
+    public void setQuerystring(String querystring) {
+        this.querystring = querystring == null ? null : querystring.trim();
+    }
+
+    public String getUseragent() {
+        return useragent;
+    }
+
+    public void setUseragent(String useragent) {
+        this.useragent = useragent == null ? null : useragent.trim();
+    }
+
+    public Integer getExectime() {
+        return exectime;
+    }
+
+    public void setExectime(Integer exectime) {
+        this.exectime = exectime;
+    }
+
+    public String getBusitype() {
+        return busitype;
+    }
+
+    public void setBusitype(String busitype) {
+        this.busitype = busitype == null ? null : busitype.trim();
+    }
+}
