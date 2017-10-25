ba43551a57cae4862e3d16de8ae7d66095e32b43
zhangl <zhangl@163.com>
2017-10-25 10:18:49 +0800

    更新状态---修改平台访问日志

--- src/main/java/com/movision/facade/h5wechat/WechatH5Facade.java
+++ src/main/java/com/movision/facade/h5wechat/WechatH5Facade.java
@@ -510,15 +510,15 @@ public class WechatH5Facade extends JPanel {
 
             g.fillRect(0, 0, getWidth(), getHeight());
             g.rotate(0, 900, 15);
-            g.drawImage(img, 0, 0, this);
+            g.drawImage(img, 540, 540, this);
             //g.rotate(30);
             //设置颜色。
             g.setColor(Color.BLACK);
 
             //最后一个参数用来设置字体的大小
-            Font f = new Font("宋体", Font.PLAIN, 20);
+            Font f = new Font("宋体", Font.PLAIN, 15);
             Color color = new Color(112, 104, 115);
-            Color[] mycolor = {color, Color.LIGHT_GRAY};
+            Color[] mycolor = {color, Color.BLACK};
             // g.setColor(mycolor);
             g.setFont(f);
             //   平移原点到图形环境的中心
@@ -530,7 +530,7 @@ public class WechatH5Facade extends JPanel {
             for (int i = 0; i < 1; i++) {
                 g.rotate(0 * Math.PI / 180, 0, 0);
                 g.setPaint(mycolor[i % 2]);
-                g.drawString(name, 130, 130);
+                g.drawString(name, 138, 130);
             }
             g.dispose();
 
