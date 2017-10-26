fd4445b3f248cf16a7dccf3e2969aff097be1a92
zhangl <zhangl@163.com>
2017-10-26 09:36:46 +0800

    更新状态--合成

--- src/main/java/com/movision/facade/h5wechat/WechatH5Facade.java
+++ src/main/java/com/movision/facade/h5wechat/WechatH5Facade.java
@@ -516,7 +516,7 @@ public class WechatH5Facade extends JPanel {
             g.setColor(Color.BLACK);
 
             //最后一个参数用来设置字体的大小
-            Font f = new Font("苹方", Font.PLAIN, 14);
+            Font f = new Font("苹方 细体", Font.PLAIN, 14);
             Color color = new Color(62, 62, 62);
             Color[] mycolor = {color, Color.BLACK};
             // g.setColor(mycolor);
