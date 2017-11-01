b26e968b7de1ac316ec0bdf11a58c209f175a7f5
ywgitlabadmin <zhuangyuhao711@163.com>
2017-11-01 13:47:36 +0800

    哈哈哈

--- src/main/java/com/movision/controller/boss/robot/RobotJobController.java
+++ src/main/java/com/movision/controller/boss/robot/RobotJobController.java
@@ -27,9 +27,9 @@ public class RobotJobController {
 
     @ApiOperation(value = "查询机器人任务列表（分页）", notes = "查询机器人任务列表（分页）", response = Response.class)
     @RequestMapping(value = "query_robot_job_page", method = RequestMethod.GET)
-    public Response queryMenuList(@RequestParam(required = false, defaultValue = "1") String pageNo,
-                                  @RequestParam(required = false, defaultValue = "10") String pageSize,
-                                  @ApiParam(value = "任务类型 。1：点赞，2：收藏，3：评论，4：关注") @RequestParam(required = false) Integer type) {
+    public Response queryRobotJobPage(@RequestParam(required = false, defaultValue = "1") String pageNo,
+                                      @RequestParam(required = false, defaultValue = "10") String pageSize,
+                                      @ApiParam(value = "任务类型 。1：点赞，2：收藏，3：评论，4：关注") @RequestParam(required = false) Integer type) {
         Response response = new Response();
         Paging<RobotOperationJobPage> pager = new Paging<>(Integer.valueOf(pageNo), Integer.valueOf(pageSize));
         List<RobotOperationJobPage> list = robotFacade.findAllRobotJobPage(type, pager);
