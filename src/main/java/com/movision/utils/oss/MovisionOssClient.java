5d59daa67d52ca48b3667a07d5dcec3653c4d9ff
zhurui <244782577@qq.com>
2017-11-01 10:51:28 +0800

    boss截图

--- src/main/java/com/movision/utils/oss/MovisionOssClient.java
+++ src/main/java/com/movision/utils/oss/MovisionOssClient.java
@@ -345,7 +345,7 @@ public class MovisionOssClient {
             incise += uuid + "." + suffix;
             System.out.println("切割图片的本体图片位置：" + incise);
             //保存新图片
-            ImageIO.write(bi, "jpg", new File(incise));
+            ImageIO.write(bi, "png", new File(incise));
             //String domain = PropertiesLoader.getValue("formal.img.domain");
             //上传本地服务器切割完成的图片到阿里云
             map = aliOSSClient.uploadInciseStream(incise, "img", "coverIncise");
@@ -372,5 +372,50 @@ public class MovisionOssClient {
                 }
         }
         return map;
+    }
+
+
+    /**
+     * 裁剪PNG图片工具类
+     *
+     * @param fromFile     源文件
+     * @param toFile       裁剪后的文件
+     * @param outputWidth  裁剪宽度
+     * @param outputHeight 裁剪高度
+     * @param proportion   是否是等比缩放
+     */
+    public static void resizePng(File fromFile, File toFile, int outputWidth, int outputHeight,
+                                 boolean proportion) {
+        try {
+            BufferedImage bi2 = ImageIO.read(fromFile);
+            int newWidth;
+            int newHeight;
+            // 判断是否是等比缩放
+            if (proportion) {
+                // 为等比缩放计算输出的图片宽度及高度
+                double rate1 = ((double) bi2.getWidth(null)) / (double) outputWidth + 0.1;
+                double rate2 = ((double) bi2.getHeight(null)) / (double) outputHeight + 0.1;
+                // 根据缩放比率大的进行缩放控制
+                double rate = rate1 < rate2 ? rate1 : rate2;
+                newWidth = (int) (((double) bi2.getWidth(null)) / rate);
+                newHeight = (int) (((double) bi2.getHeight(null)) / rate);
+            } else {
+                newWidth = outputWidth; // 输出的图片宽度
+                newHeight = outputHeight; // 输出的图片高度
+            }
+            BufferedImage to = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
+            Graphics2D g2d = to.createGraphics();
+            to = g2d.getDeviceConfiguration().createCompatibleImage(newWidth, newHeight,
+                    Transparency.TRANSLUCENT);
+            g2d.dispose();
+            g2d = to.createGraphics();
+            @SuppressWarnings("static-access")
+            Image from = bi2.getScaledInstance(newWidth, newHeight, bi2.SCALE_AREA_AVERAGING);
+            g2d.drawImage(from, 0, 0, null);
+            g2d.dispose();
+            ImageIO.write(to, "png", toFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
