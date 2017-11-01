0945e7cf63d5875fcfee261b4d27c695600254e9
shuxf <shuxianfon1987@126.com>
2017-11-01 15:42:53 +0800

    重构多图分层压缩逻辑代码

--- src/main/java/com/movision/utils/JsoupCompressImg.java
+++ src/main/java/com/movision/utils/JsoupCompressImg.java
@@ -132,24 +132,9 @@ public class JsoupCompressImg {
                     //根据不同图片的宽高程度进行不同程度的压缩
                     int prow = image.getWidth();//原图宽
                     int proh = image.getHeight();//原图高
-                    int w = 0;//压缩后宽
-                    int h = 0;//压缩后高
-                    if (prow < 1000 && proh < 1000){//宽高都在1000px一下，尺寸不压缩
-                        w = prow;
-                        h = proh;
-                    }else if ((prow < 2000 && prow >= 1000) || (proh < 2000 && proh >= 1000)){//宽高有在1000~2000px的压缩到原尺寸的80%
-                        w = (int) (prow * 0.80);
-                        h = (int) (proh * 0.80);
-                    }else if ((prow < 3000 && prow >= 2000) || (proh < 3000 && proh >= 2000)){//宽高有在2000~3000px的压缩到原尺寸的60%
-                        w = (int) (prow * 0.60);
-                        h = (int) (proh * 0.60);
-                    }else if ((prow < 4000 && prow >= 3000) || (proh < 4000 && proh >= 3000)){//宽高有在3000~4000px的压缩到原尺寸的40%
-                        w = (int) (prow * 0.40);
-                        h = (int) (proh * 0.40);
-                    }else if (prow > 5000 || proh > 5000) {//宽高有在5000px以上的压缩到原尺寸的20%
-                        w = (int) (image.getWidth() * 0.20);
-                        h = (int) (image.getHeight() * 0.20);
-                    }
+                    Double rate = getWH(prow, proh);//计算压缩比例
+                    int w = (int) (prow * rate);//压缩后宽
+                    int h = (int) (proh * rate);//压缩后高
 
                     if (StringUtils.isNotEmpty(imgurl)) {
 
@@ -328,24 +313,9 @@ public class JsoupCompressImg {
                         //根据不同图片的宽高程度进行不同程度的压缩
                         int prow = image.getWidth();//原图宽
                         int proh = image.getHeight();//原图高
-                        int w = 0;//压缩后宽
-                        int h = 0;//压缩后高
-                        if (prow < 1000 && proh < 1000){//宽高都在1000px一下，尺寸不压缩
-                            w = prow;
-                            h = proh;
-                        }else if ((prow < 2000 && prow >= 1000) || (proh < 2000 && proh >= 1000)){//宽高有在1000~2000px的压缩到原尺寸的80%
-                            w = (int) (prow * 0.80);
-                            h = (int) (proh * 0.80);
-                        }else if ((prow < 3000 && prow >= 2000) || (proh < 3000 && proh >= 2000)){//宽高有在2000~3000px的压缩到原尺寸的60%
-                            w = (int) (prow * 0.60);
-                            h = (int) (proh * 0.60);
-                        }else if ((prow < 4000 && prow >= 3000) || (proh < 4000 && proh >= 3000)){//宽高有在3000~4000px的压缩到原尺寸的40%
-                            w = (int) (prow * 0.40);
-                            h = (int) (proh * 0.40);
-                        }else if (prow > 5000 || proh > 5000) {//宽高有在5000px以上的压缩到原尺寸的20%
-                            w = (int) (image.getWidth() * 0.20);
-                            h = (int) (image.getHeight() * 0.20);
-                        }
+                        Double rate = getWH(prow, proh);//计算压缩比例
+                        int w = (int) (prow * rate);//压缩后宽
+                        int h = (int) (proh * rate);//压缩后高
 
                         if (StringUtils.isNotEmpty(imgurl)) {
 
@@ -488,5 +458,28 @@ public class JsoupCompressImg {
             e.printStackTrace();
         }
         return existFileList;
+    }
+
+    /**
+     * 抽象图片分层压缩的逻辑方法代码
+     * 根据原图宽高判断不同压缩比例
+     * @param prow
+     * @param proh
+     * @return
+     */
+    public Double getWH(int prow, int proh){
+        Double rate = 0.00;
+        if (prow < 1000 && proh < 1000){//宽高都在1000px一下，尺寸不压缩
+            rate = 1.00;
+        }else if ((prow < 2000 && prow >= 1000) || (proh < 2000 && proh >= 1000)){//宽高有在1000~2000px的压缩到原尺寸的80%
+            rate = 0.80;
+        }else if ((prow < 3000 && prow >= 2000) || (proh < 3000 && proh >= 2000)){//宽高有在2000~3000px的压缩到原尺寸的60%
+            rate = 0.60;
+        }else if ((prow < 4000 && prow >= 3000) || (proh < 4000 && proh >= 3000)){//宽高有在3000~4000px的压缩到原尺寸的40%
+            rate = 0.40;
+        }else if (prow > 5000 || proh > 5000) {//宽高有在5000px以上的压缩到原尺寸的20%
+            rate = 0.20;
+        }
+        return rate;
     }
 }
