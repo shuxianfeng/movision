3f1cb2afb37aa8b0a82533c69218e9deb5c4919e
zhurui <244782577@qq.com>
2017-11-07 18:09:35 +0800

    xml

--- src/main/java/com/movision/facade/boss/XmlParseFacade.java
+++ src/main/java/com/movision/facade/boss/XmlParseFacade.java
@@ -574,7 +574,13 @@ public class XmlParseFacade {
             if (substring[i].substring(0, substring[i].indexOf(":")).equals("orign")) {
                 content += "\"type\":1,";
                 //图片处理
-                Map m = download(substring[i].substring(substring[i].indexOf(":") + 1, substring[i].indexOf("?")), "img");
+                String s = "";
+                if (substring[i].length() - 1 == substring[i].lastIndexOf("?")) {
+                    s = substring[i].substring(substring[i].indexOf(":") + 1, substring[i].indexOf("?"));
+                } else {
+                    s = substring[i].substring(substring[i].indexOf(":") + 1, substring[i].length());
+                }
+                Map m = download(s, "img");
                 //获取本地文件
                 list.add(m.get("oldurl"));
                 //帖子封面处理
