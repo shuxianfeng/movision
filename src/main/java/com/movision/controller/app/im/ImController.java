12d366c80040676da16978ddc19f4ee71d320fc6
ywgitlabadmin <zhuangyuhao711@163.com>
2017-11-08 15:33:04 +0800

    哈哈哈

--- src/main/java/com/movision/controller/app/im/ImController.java
+++ src/main/java/com/movision/controller/app/im/ImController.java
@@ -25,42 +25,11 @@ public class ImController {
     @Autowired
     private ImFacade imFacade;
 
-    /**
-     * 打招呼接口
-     *
-     * @return
-     * @throws IOException
-     */
     @ApiOperation(value = "发消息", notes = "发消息", response = Response.class)
     @RequestMapping(value = "/send_msg", method = RequestMethod.POST)
     public Response sayHi(@ApiParam @ModelAttribute ImMsg imMsg) throws IOException {
         Response response = new Response();
         imFacade.sendImMsg(imMsg);
-        return response;
-    }
-
-
-    @ApiOperation(value = "更新用户最新消息为已读", notes = "更新用户最新消息为已读", response = Response.class)
-    @RequestMapping(value = "/update_new_informations", method = RequestMethod.POST)
-    public Response UpdateNewInformations(@ApiParam(value = "登录用户id") @RequestParam String userid) {
-        Response response = new Response();
-        // TODO: 2017/9/19 使用mongo记录
-        if (response.getCode() == 200) {
-            response.setMessage("操作成功");
-        }
-        return response;
-    }
-
-
-    @ApiOperation(value = "查询用户最新消息", notes = "查询用户最新消息", response = Response.class)
-    @RequestMapping(value = "/query_new_informations", method = RequestMethod.POST)
-    public Response QueryNewInformations(@ApiParam(value = "登录用户id") @RequestParam String userid) {
-        Response response = new Response();
-        // TODO: 2017/9/19 使用mongo记录
-        if (response.getCode() == 200) {
-            response.setMessage("操作成功");
-        }
-
         return response;
     }
 
