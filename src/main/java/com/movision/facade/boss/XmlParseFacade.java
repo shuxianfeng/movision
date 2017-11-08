bb44e487281e813c465d99d356a8f8eb49906af7
zhurui <244782577@qq.com>
2017-11-08 16:35:32 +0800

    xml

--- src/main/java/com/movision/facade/boss/XmlParseFacade.java
+++ src/main/java/com/movision/facade/boss/XmlParseFacade.java
@@ -702,7 +702,7 @@ public class XmlParseFacade {
             }
             content += "{\"type\": 0,\"orderid\":" + num + ",\"value\":\"" + caps + "\",\"wh\": \"\",\"dir\": \"\"}]";*/
         } catch (Exception e1) {
-            e1.printStackTrace();
+            return "";
         }
         return content;
     }
