df4849d37ae87973387a4f885e3cad00686b74c5
shuxf <shuxianfon1987@126.com>
2018-01-15 15:00:41 +0800

    开放BOSS端的日志记录

--- src/main/java/com/movision/aop/AspectLog.java
+++ src/main/java/com/movision/aop/AspectLog.java
@@ -11,6 +11,7 @@ import org.apache.shiro.subject.Subject;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,6 +39,8 @@ public class AspectLog {
     private static final Logger log = LoggerFactory.getLogger(AspectLog.class);
 
     ThreadLocal<Long> time = new ThreadLocal<Long>();
+
+    @Pointcut("!execution(* com.movision.controller.boss.query*(..))")
 
     @Around("@annotation(org.springframework.web.bind.annotation.RequestMapping)")
     public Object aroundLog(ProceedingJoinPoint pjp) throws Throwable {
