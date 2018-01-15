5be20b81c88393b39e6db4afee67bb37170523fd
shuxf <shuxianfon1987@126.com>
2018-01-15 15:24:36 +0800

    开放BOSS端的日志记录

--- src/main/java/com/movision/aop/AspectLog.java
+++ src/main/java/com/movision/aop/AspectLog.java
@@ -40,9 +40,12 @@ public class AspectLog {
 
     ThreadLocal<Long> time = new ThreadLocal<>();
 
-//    @Pointcut("!execution(* com.movision.controller.boss.query*(..))")
+    @Pointcut("!execution(* com.movision.controller.boss.query*(..))")
+    public void pointCut(){
+    }
 
-    @Around("@annotation(org.springframework.web.bind.annotation.RequestMapping)")
+//    @Around("@annotation(org.springframework.web.bind.annotation.RequestMapping)")
+    @Around("pointCut()")
     public Object aroundLog(ProceedingJoinPoint pjp) throws Throwable {
         log.info("测试是否进入AOP方法");
         time.set(System.currentTimeMillis());
