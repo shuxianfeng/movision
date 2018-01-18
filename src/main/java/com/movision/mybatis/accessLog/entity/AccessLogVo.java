58bd5eb52410c8fe09bf5d1a293fc6ad016e92bb
zhurui <244782577@qq.com>
2018-01-18 15:36:46 +0800

    更新

---
+++ src/main/java/com/movision/mybatis/accessLog/entity/AccessLogVo.java
@@ -0,0 +1,115 @@
+package com.movision.mybatis.accessLog.entity;
+
+import java.util.Date;
+
+public class AccessLogVo {
+    private Integer id;
+
+    private Integer memberid;
+
+    private String clientip;
+
+    private String httpmethod;
+
+    private String requesturl;
+
+    private String querystring;
+
+    private String useragent;
+
+    private Integer exectime;
+
+    private String busitype;
+
+    private Date intime;
+
+    private String nickname;
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public Date getIntime() {
+        return intime;
+    }
+
+    public void setIntime(Date intime) {
+        this.intime = intime;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getMemberid() {
+        return memberid;
+    }
+
+    public void setMemberid(Integer memberid) {
+        this.memberid = memberid;
+    }
+
+    public String getClientip() {
+        return clientip;
+    }
+
+    public void setClientip(String clientip) {
+        this.clientip = clientip == null ? null : clientip.trim();
+    }
+
+    public String getHttpmethod() {
+        return httpmethod;
+    }
+
+    public void setHttpmethod(String httpmethod) {
+        this.httpmethod = httpmethod == null ? null : httpmethod.trim();
+    }
+
+    public String getRequesturl() {
+        return requesturl;
+    }
+
+    public void setRequesturl(String requesturl) {
+        this.requesturl = requesturl == null ? null : requesturl.trim();
+    }
+
+    public String getQuerystring() {
+        return querystring;
+    }
+
+    public void setQuerystring(String querystring) {
+        this.querystring = querystring == null ? null : querystring.trim();
+    }
+
+    public String getUseragent() {
+        return useragent;
+    }
+
+    public void setUseragent(String useragent) {
+        this.useragent = useragent == null ? null : useragent.trim();
+    }
+
+    public Integer getExectime() {
+        return exectime;
+    }
+
+    public void setExectime(Integer exectime) {
+        this.exectime = exectime;
+    }
+
+    public String getBusitype() {
+        return busitype;
+    }
+
+    public void setBusitype(String busitype) {
+        this.busitype = busitype == null ? null : busitype.trim();
+    }
+}
