fdbc6b6634072d064b6ad36aaa8c62862d505857
shuxf <shuxianfon1987@126.com>
2018-02-02 10:16:50 +0800

    使用登录凭证code获取session_key和openid

--- src/main/java/com/movision/controller/app/wechat/WxLoginController.java
+++ src/main/java/com/movision/controller/app/wechat/WxLoginController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 /**
  * @Author shuxf
  * @Date 2018/2/1 16:42
